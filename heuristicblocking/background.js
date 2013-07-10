ba1ebedae132a198fa9d6f7ea30b6cef4bd3166e
Jonathan Mayer <jmayer@stanford.edu>
2013-07-10 14:18:59 -0700

    added heuristic implementation

--- heuristicblocking/background.js
+++ heuristicblocking/background.js
@@ -1,14 +1,18 @@
-tabOrigins = { };
+var tabOrigins = { };
+var originFrequency = { };
+
+var prevalenceThreshold = 3;
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
 	// Ignore requests that are outside a tabbed window
 	if(details.tabId < 0)
 		return { };
 	
-	var origin = getBaseDomain(details.url);
+	var origin = getBaseDomain(new URI(details.url).host);
 	
 	// Save the origin associated with the tab if this is a main window request
 	if(details.type == "main_frame") {
+		console.log("Origin: " + origin + "\tURL: " + details.url);
 		tabOrigins[details.tabId] = origin;
 		return { };
 	}
@@ -16,8 +20,16 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 		var tabOrigin = tabOrigins[details.tabId];
 		if (origin == tabOrigin)
 			return { };
-		else
-			return { cancel: true };
+		else {
+			if(!(origin in originFrequency))
+				originFrequency[origin] = { };
+			originFrequency[origin][tabOrigin] = true;
+			if(Object.keys(originFrequency[origin]).length >= prevalenceThreshold) {
+				console.log("Blocked " + origin + " because it appeared on: " + Object.keys(originFrequency[origin]));
+				return { cancel: true };
+			}
+			return { };
+		}
 	}
 },
 {urls: ["<all_urls>"]},
