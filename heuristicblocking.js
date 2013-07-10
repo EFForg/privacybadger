6d35695910c8941025a4c9a960ee2f9e8cf18666
Peter Eckersley <pde@eff.org>
2013-07-10 15:37:04 -0700

    Work in progress

--- heuristicblocking.js
+++ heuristicblocking.js
@@ -1,7 +1,28 @@
+with(require("filterClasses"))
+{
+  this.Filter = Filter;
+  this.RegExpFilter = RegExpFilter;
+  this.BlockingFilter = BlockingFilter;
+  this.WhitelistFilter = WhitelistFilter;
+}
+var FilterStorage = require("filterStorage").FilterStorage;
 var tabOrigins = { };
 var originFrequency = { };
 
 var prevalenceThreshold = 3;
+
+blacklistOrigin = function(origin) {
+  let filter = this.Filter.fromText("||" + origin + "^$third-party");
+  filter.disabled = false;
+  this.FilterStorage.addFilter(filter);
+  console.log("Nothing exploded while blacklisting " + origin);
+
+  //if (nodes)
+  //  Policy.refilterNodes(nodes, item);
+
+  return true;
+}
+
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
   // Ignore requests that are outside a tabbed window
@@ -24,8 +45,15 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
       if(!(origin in originFrequency))
         originFrequency[origin] = { };
       originFrequency[origin][tabOrigin] = true;
-      if(Object.keys(originFrequency[origin]).length >= prevalenceThreshold) {
+      let l = Object.keys(originFrequency[origin]).length;
+      if( l >= prevalenceThreshold) {
         console.log("Blocked " + origin + " because it appeared on: " + Object.keys(originFrequency[origin]));
+        if (l ==prevalenceThreshold) {
+          console.log("Step 1");
+          blacklistOrigin(origin);
+          console.log("Step 2");
+        }
+
         return { cancel: true };
       }
       return { };
