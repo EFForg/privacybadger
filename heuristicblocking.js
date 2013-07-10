7e657ebb962a2e0cd22a5a7299b7f5851b47da05
Peter Eckersley <pde@eff.org>
2013-07-10 16:39:25 -0700

    Keep fighting against evil tabs

--- heuristicblocking.js
+++ heuristicblocking.js
@@ -5,61 +5,61 @@ var originFrequency = { };
 var prevalenceThreshold = 3;
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-	// Ignore requests that are outside a tabbed window
-	if(details.tabId < 0)
-		return { };
-	
-	var origin = getBaseDomain(new URI(details.url).host);
-	
-	// Save the origin associated with the tab if this is a main window request
-	if(details.type == "main_frame") {
-		//console.log("Origin: " + origin + "\tURL: " + details.url);
-		tabOrigins[details.tabId] = origin;
-		return { };
-	}
-	
-	else {
-		var tabOrigin = tabOrigins[details.tabId];
-		if (origin == tabOrigin)
-			return { };
-		else if(!(origin in originFrequency))
-			return { };
-		else {
-			if(Object.keys(originFrequency[origin]).length >= prevalenceThreshold) {
-				console.log("Blocked " + origin + " because it appeared with cookies on: " + Object.keys(originFrequency[origin]));
-				return { cancel: true };
-			}
-			return { };
-		}
-	}
+  // Ignore requests that are outside a tabbed window
+  if(details.tabId < 0)
+    return { };
+  
+  var origin = getBaseDomain(new URI(details.url).host);
+  
+  // Save the origin associated with the tab if this is a main window request
+  if(details.type == "main_frame") {
+    //console.log("Origin: " + origin + "\tURL: " + details.url);
+    tabOrigins[details.tabId] = origin;
+    return { };
+  }
+  
+  else {
+    var tabOrigin = tabOrigins[details.tabId];
+    if (origin == tabOrigin)
+      return { };
+    else if(!(origin in originFrequency))
+      return { };
+    else {
+      if(Object.keys(originFrequency[origin]).length >= prevalenceThreshold) {
+        console.log("Blocked " + origin + " because it appeared with cookies on: " + Object.keys(originFrequency[origin]));
+        return { cancel: true };
+      }
+      return { };
+    }
+  }
 },
 {urls: ["<all_urls>"]},
 ["blocking"]);
 
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-	// Ignore requests that are outside a tabbed window
-	if(details.tabId < 0)
-		return { };
-	
-	// Log the visit if a cookie was sent
-	var hasCookie = false;
-	for(var i = 0; i < details.requestHeaders.length; i++) {
-		if(details.requestHeaders[i].name == "Cookie") {
-			hasCookie = true;
-			break;
-		}
-	}
-	if(hasCookie) {
-		var origin = getBaseDomain(new URI(details.url).host);
-		var tabOrigin = tabOrigins[details.tabId];
-		if (origin != tabOrigin) {
-			if(!(origin in originFrequency))
-				originFrequency[origin] = { };
-			originFrequency[origin][tabOrigin] = true;
-		}
-	}
-	
-	return { };
+  // Ignore requests that are outside a tabbed window
+  if(details.tabId < 0)
+    return { };
+  
+  // Log the visit if a cookie was sent
+  var hasCookie = false;
+  for(var i = 0; i < details.requestHeaders.length; i++) {
+    if(details.requestHeaders[i].name == "Cookie") {
+      hasCookie = true;
+      break;
+    }
+  }
+  if(hasCookie) {
+    var origin = getBaseDomain(new URI(details.url).host);
+    var tabOrigin = tabOrigins[details.tabId];
+    if (origin != tabOrigin) {
+      if(!(origin in originFrequency))
+        originFrequency[origin] = { };
+      originFrequency[origin][tabOrigin] = true;
+    }
+  }
+  
+  return { };
 },
 {urls: ["<all_urls>"]},
 ["requestHeaders"]);
