768f379948bbd403b1bff87c4769fb8174d4a797
Dan Auerbach <dan@eff.org>
2013-07-14 21:36:42 -0700

    Add schema for test data scraping

---
+++ dataschema.sql
@@ -0,0 +1,13 @@
+-- Schema for privacy badger data collection
+
+CREATE TABLE IF NOT EXISTS reports (
+  report_id int(11) NOT NULL AUTO_INCREMENT,
+  user_id int(11) DEFAULT NULL,
+  origin varchar(255) NOT NULL,
+  thirdpartynum int(11),
+  cookiesentnum int(11),
+  cookiereceivednum int(11),
+  timestamp DATETIME,
+  latest tinyint(1),
+) ENGINE=MyISAM DEFAULT CHARSET-UTF8; 
+  
