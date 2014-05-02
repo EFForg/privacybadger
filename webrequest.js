07f3f3171ffcd71ad869f90bd38f8b93d1efa990
cooperq <cooperq@cooperq.com>
2014-05-02 12:09:52 -0700

    show all 3rd party domains in badger popup to address issues #63 and #113

--- webrequest.js
+++ webrequest.js
@@ -363,9 +363,9 @@ function checkRequest(type, tabId, url, frameId) {
     var blockedData = activeMatchers.blockedOriginsByTab[tabId];
     var blockedDataByHost = blockedData[requestHost];
     if (!(blockedDataByHost)){
-      /*// if the third party origin is not blocked we add it to the list to 
-      // inform the user of all trackers.
-      activeMatchers.addMatcherToOrigin(tabId, requestHost, false, false);*/
+      // if the third party origin is not blocked we add it to the list to 
+      // inform the user of all third parties on the page.
+      activeMatchers.addMatcherToOrigin(tabId, requestHost, false, false);
       return "noaction";
     }
     //console.log("Subscription data for " + requestHost + " is: " + JSON.stringify(blockedData[requestHost]));
