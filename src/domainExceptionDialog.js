713f0128bbac75b3810f81d7110a4fe00eec88db
Peter Eckersley <pde@eff.org>
2014-07-16 14:49:41 -0700

    Tweak wording

--- src/domainExceptionDialog.js
+++ src/domainExceptionDialog.js
@@ -10,7 +10,7 @@ chrome.runtime.onConnect.addListener(
       '<div id="pbLogo"><img src="' + chrome.extension.getURL("icons/badger-48.png") + '"></div>'+
       '<h2>Privacy Badger Alert!</h2>' +
       '<div class="clear"></div>' +
-      '<h3>Logging into ' + msg.whitelistDomain + ' can allow them to track you around the web.</h3>' +
+      '<h3>Logging into ' + msg.whitelistDomain + ' can allow it to track you around the web.</h3>' +
       '<button class="pbButton default" id="allow_once">Only allow ' + msg.whitelistDomain + ' on ' + msg.currentDomain + '</button>' +
       '<button class="pbButton" id="allow_all">Always allow ' + msg.whitelistDomain + '</button>' +
       '<button class="pbButton" id="never">Continue blocking ' + msg.whitelistDomain + ' for now</button>' +
