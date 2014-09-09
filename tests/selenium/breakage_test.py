e479d6efe140d9bcd25137a8aefae037fd3800c3
Gunes Acar <gunes.acar@esat.kuleuven.be>
2014-09-08 18:14:44 -0700

    Add comments for breakage tests

--- tests/selenium/breakage_test.py
+++ tests/selenium/breakage_test.py
@@ -7,6 +7,11 @@ from time import sleep
 
 
 class Test(pbtest.PBSeleniumTest):
+    """Make sure we don't badly break things when we install the extension.
+    e.g. we should be able to load a website, search on Google.
+    TODO: Add other tests to simulate most common web use cases:
+    e.g. play Youtube videos, login to popular services, tweet some text,
+    add Reddit comments etc."""
 
     def test_should_load_eff_org(self):
         self.driver.get("https://www.eff.org")
