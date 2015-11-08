bb7b195e86c1e6682e9342098c5d48b7470d4752
tinkerb <lesley@lesleybell.com>
2015-11-07 19:52:30 -0800

    new tutorial overlay for new users

---
+++ src/tutorial.js
@@ -0,0 +1,5 @@
+var thing = document.getElementById("instruction");
+var escapeThing = document.getElementById("escape");
+escapeThing.addEventListener('click', function() {
+	thing.style.display = 'none';
+})
