89a16b34c5b2804e24c2c019f116907754d27cf9
pollyp <pollyp@gmail.com>
2015-12-16 13:51:14 -0800

    for travis setup, bump chromedriver version to 2.20 and install the latest stable Chrome

--- setup_travis.sh
+++ setup_travis.sh
@@ -10,8 +10,14 @@ else
     elif [ $machine = "i686" ] ; then 
         bits="32"; 
     fi;
-    version="2.12"
+    version="2.20"
     wget -O /tmp/chromedriver.zip "https://chromedriver.storage.googleapis.com/$version/chromedriver_linux$bits.zip"
     sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
     sudo chmod a+x /usr/local/bin/chromedriver
 fi
+
+# Install latest stable Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
