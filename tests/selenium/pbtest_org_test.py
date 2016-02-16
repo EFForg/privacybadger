f31a9ba4e27695535ae1d616eb358635e37ee04e
pollyp <pollyp@gmail.com>
2016-02-16 05:14:56 -0800

    removed IPython import

--- tests/selenium/pbtest_org_test.py
+++ tests/selenium/pbtest_org_test.py
@@ -6,7 +6,7 @@ import sys
 import time
 import unittest
 
-import IPython
+#import IPython
 
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
 
