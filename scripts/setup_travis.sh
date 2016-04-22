6a93e0e10cb5c9a5579f9d3693dc47c7d3820796
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-04-22 22:29:16 +0200

    Upgrade Ubuntu on Travis to 14.04.
    Install Chrome using Travis APT addon.
    Read latest version of ChromeDriver from its release page.
    
    Remove redundant check about the machine architecture.
    
    Do not install xvfb, as it's already installed on Travis machines.
    
    Run py.test with -v switch.

--- scripts/setup_travis.sh
+++ scripts/setup_travis.sh
@@ -4,20 +4,8 @@
 if [ -x /usr/local/bin/chromedriver ]; then
     echo "You already have ChromeDriver installed. Skipping this step."
 else
-    machine=`uname -m`
-    if [ $machine = "x86_64" ] ; then 
-        bits="64"; 
-    elif [ $machine = "i686" ] ; then 
-        bits="32"; 
-    fi;
-    version="2.20"
-    wget -O /tmp/chromedriver.zip "https://chromedriver.storage.googleapis.com/$version/chromedriver_linux$bits.zip"
+    latest_version=$(wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE -q -O -)
+    wget -O /tmp/chromedriver.zip "https://chromedriver.storage.googleapis.com/${latest_version}/chromedriver_linux64.zip"
     sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
     sudo chmod a+x /usr/local/bin/chromedriver
 fi
-
-# Install latest stable Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable
