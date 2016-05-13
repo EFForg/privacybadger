c96b10dac1ff45488afe4ea6cbbd49689485e892
Cooper Quintin <cooperq@eff.org>
2016-05-13 15:58:04 -0700

    fixing run nightly script

--- scripts/run-nightly.sh
+++ scripts/run-nightly.sh
@@ -2,5 +2,5 @@
 PROFILEDIR=/tmp/tmp-fx-profile.$$.d
 
 mkdir $PROFILEDIR
-~/Downloads/firefox/firefox -profile $PROFILEDIR -no-remote -new-instance
+~/bin/firefox/firefox -profile $PROFILEDIR -no-remote -new-instance
 rm -rf $PROFILEDIR
