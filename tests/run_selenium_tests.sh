2bd3a7c21ad41e5db66a4f40e1f0ec6a18a948e8
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-09-20 20:34:52 +0200

    Run Travis tests for Chrome beta channel.
    Allow failures for the beta version.
    Enable fast finishing.

--- tests/run_selenium_tests.sh
+++ tests/run_selenium_tests.sh
@@ -15,9 +15,10 @@ pip install -r sel_requirements.txt
 # TODO: take command line arguments to set the following environment variables
 export PB_EXT_PATH=$ext_path  # extension on this path will be used in the tests
 # if this var is empty, extension base dir will be searched for the last modified .crx.
-export BROWSER_BIN=""   # Path to the browser binary. Optional.
-# If empty, Selenium will pick the default binary for the selected browser.
-# To run tests with Chromium (instead of default Google Chrome) export BROWSER_BIN="/usr/bin/chromium-browser"
+echo "Chrome path: "$BROWSER_BIN
+# export BROWSER_BIN="/path/to/chrome"   # Optional.
+# If BROWSER_BIN is empty, Selenium will pick the default binary for Chrome.
+# To run tests with Chromium (instead of Google Chrome) export BROWSER_BIN="/usr/bin/chromium-browser"
 export ENABLE_XVFB=1    # run the tests headless using Xvfb. Set 0 to disable
 # for i in {1..20}; do echo "Run "$i; py.test -s -v --durations=10 selenium; done  # autodiscover and run the tests
 py.test -s -v --durations=10 selenium  # autodiscover and run the tests
