7e2711b7ce892e273f4a471fe09bea58e7f31ecd
Alex Ristich <alexristich@gmail.com>
2016-10-07 01:12:16 -0700

    Updating initial check to falsy statement

--- src/socialwidgets.js
+++ src/socialwidgets.js
@@ -287,7 +287,7 @@ function replaceInitialTrackerButtonsHelper(trackerButtonsToReplace) {
  * Individually replaces tracker buttons blocked after initial check.
  */
 function replaceSubsequentTrackerButtonsHelper(trackerDomain) {
-  if (trackerInfo === undefined || trackerInfo.length === 0) { return; }
+  if (!trackerInfo) { return; }
   trackerInfo.forEach(function(tracker) {
     var replaceTrackerButtons = (tracker.domain == trackerDomain);
     if (replaceTrackerButtons) {
