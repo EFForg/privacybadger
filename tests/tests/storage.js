a9179501092d256e76c42c135333923c6a43c95c
Alexei <alexeiatyahoodotcom@gmail.com>
2016-11-10 17:00:51 -0500

    Remove unneeded initialization.
    Storage already initialized earlier in background.js

--- tests/tests/storage.js
+++ tests/tests/storage.js
@@ -2,49 +2,44 @@
 (function() {
   module("Privacy Badger Storage");
 
-  var BadgerStore = require('storage');
+  test("testGetBadgerStorage", function(){
+    expect(1);
+    var action_map = badger.storage.getBadgerStorageObject('action_map');
+    ok(action_map.updateObject instanceof Function, "action_map is a pbstorage");
+  });
 
-  new BadgerStore.BadgerPen(function(){ // eslint-disable-line no-new
-  
-    test("testGetBadgerStorage", function(){
-      expect(1);
-      var action_map = badger.storage.getBadgerStorageObject('action_map');
-      ok(action_map.updateObject instanceof Function, "action_map is a pbstorage");
-    });
+  test("test BadgerStorage methods", function(){
+    expect(3);
+    var action_map = badger.storage.getBadgerStorageObject('action_map');
+    action_map.setItem('foo', 'bar');
+    ok(action_map.getItem('foo') === 'bar');
+    ok(action_map.hasItem('foo'));
+    action_map.deleteItem('foo');
+    ok(!action_map.hasItem('foo'));
+  });
 
-    test("test BadgerStorage methods", function(){
-      expect(3);
-      var action_map = badger.storage.getBadgerStorageObject('action_map');
-      action_map.setItem('foo', 'bar');
-      ok(action_map.getItem('foo') === 'bar');
-      ok(action_map.hasItem('foo'));
-      action_map.deleteItem('foo'); 
-      ok(!action_map.hasItem('foo'));
-    });
+  test("test user override of default action for domain", function(){
+    expect(4);
+    badger.saveAction("allow", "pbtest.org");
+    ok(badger.userAllow.indexOf('pbtest.org') > -1);
+    badger.saveAction("block", "pbtest.org");
+    ok(badger.userAllow.indexOf('pbtest.org') <= -1);
+    badger.saveAction("allow", "pbtest.org");
+    ok(badger.userAllow.indexOf('pbtest.org') > -1);
+    badger.storage.revertUserAction("pbtest.org");
+    ok(badger.userAllow.indexOf('pbtest.org') <= -1);
+  });
 
-    test("test user override of default action for domain", function(){
-      expect(4);
-      badger.saveAction("allow", "pbtest.org");
-      ok(badger.userAllow.indexOf('pbtest.org') > -1);
-      badger.saveAction("block", "pbtest.org");
-      ok(badger.userAllow.indexOf('pbtest.org') <= -1);
-      badger.saveAction("allow", "pbtest.org");
-      ok(badger.userAllow.indexOf('pbtest.org') > -1);
-      badger.storage.revertUserAction("pbtest.org");
-      ok(badger.userAllow.indexOf('pbtest.org') <= -1);
-    });
-
-    test("data persists to local storage", function(){
-      // TODO: Figure out how to test this. 
-      expect(1); //expect 1 assertion
-      /*var action_map = BadgerStore.getBadgerStorageObject('action_map');
-      action_map.setItem('foo', 'bar');
-      setTimeout(function(){
-        var data = JSON.parse(localStorage.getItem('action_map'));
-        ok(data.foo == 'bar', "data presists to local storage");
-        start();
-      }, 500);*/
-      ok(true);
-    });
+  test("data persists to local storage", function(){
+    // TODO: Figure out how to test this.
+    expect(1); //expect 1 assertion
+    /*var action_map = BadgerStore.getBadgerStorageObject('action_map');
+    action_map.setItem('foo', 'bar');
+    setTimeout(function(){
+      var data = JSON.parse(localStorage.getItem('action_map'));
+      ok(data.foo == 'bar', "data persists to local storage");
+      start();
+    }, 500);*/
+    ok(true);
   });
 })();
