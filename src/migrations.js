f471b5ca6782f1b49d3f538616060cd536f2b4f4
Cooper Quintin <cooperq@eff.org>
2017-01-23 14:46:53 -0800

    remove firefox migration code

--- src/migrations.js
+++ src/migrations.js
@@ -47,72 +47,7 @@ exports.Migrations= {
     }, 1000 * 30);
   },
 
-  migrateLegacyFirefoxData: function(badger){
-    var legacyKeys = ["cookieDb","domainExceptionSites","userRed","userYellow","userGreen","disabledSites","disabledSitesPrivate","domainExceptions","domainExceptionsPrivate","seenComic","changedCookies","preloads","originFrequency","originFrequencyPrivate","blockedOrigins","policyHashes","policyWhitelist","nextBlockedSitesCheck"];
-    chrome.storage.local.get(legacyKeys, function(legacyStorage){
-      if(legacyStorage){
-        console.log('LEGACY', legacyStorage);
-      }
-
-      if(_.isEmpty(legacyStorage)){
-        console.log("No legacy firefox data found. Nothing to migrate");
-        return; 
-      }
-
-      console.log('MIGRATING FIREFOX DATA', legacyStorage);
-
-      var originFrequency = legacyStorage.originFrequency;
-      var disabledSites = legacyStorage.disabledSites;
-      var policyWhitelist = legacyStorage.policyWhitelist;
-      var userGreen = legacyStorage.userGreen;
-      var userYellow = legacyStorage.userYellow;
-      var userRed = legacyStorage.userRed;
-
-      // Import snitch map 
-      _.each(originFrequency, function(fpDomains, trackingDomain){
-        _.each(fpDomains, function(v, firstParty){
-          badger.heuristicBlocking.recordPrevalence(
-            trackingDomain,
-            trackingDomain,
-            firstParty
-          );
-        });
-      });
-
-      // Import action map
-      _.each(userGreen, function(v, domain){
-        badger.storage.setupUserAction(domain, constants.USER_ALLOW);
-      });
-
-      _.each(userYellow, function(v, domain){
-        badger.storage.setupUserAction(domain, constants.USER_COOKIE_BLOCK);
-      });
-      
-      _.each(userRed, function(v, domain){
-        badger.storage.setupUserAction(domain, constants.USER_BLOCK);
-      });
-
-      _.each(policyWhitelist, function(v, domain){
-        badger.checkForDNTPolicy(domain, 0);
-      });
-
-      // Import disabled sites and set seen comic flag
-      _.each(disabledSites, function(v, domain){
-        badger.disablePrivacyBadgerForOrigin(domain);
-      });
-
-      var settings = badger.storage.getBadgerStorageObject("settings_map");
-      settings.setItem("seenComic", true);
-      settings.setItem("isFirstRun", false);
-
-      // Cleanup
-      chrome.storage.local.remove(Object.keys(legacyStorage), function(){
-        console.log("Finished migrating storage. Cleaned up on my way out.");
-      });
-
-    });
-     
-  },
+  migrateLegacyFirefoxData: function(){ },
 
   migrateDntRecheckTimes: function(badger){
     var action_map = badger.storage.getBadgerStorageObject('action_map');
