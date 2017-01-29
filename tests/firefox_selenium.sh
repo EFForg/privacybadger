d2fa460078567e76998a6b64f269f2d5266967c9
Blake Griffith <cowlicks@riseup.net>
2017-01-28 20:57:57 -0800

    fix path in firefox test script

--- tests/firefox_selenium.sh
+++ tests/firefox_selenium.sh
@@ -2,7 +2,7 @@
 set -e
 ffpath=/usr/bin/firefox-aurora
 
-node_modules/web-ext/bin/web-ext run --firefox=$ffpath --pref marionette.defaultPrefs.enabled=true &
+../node_modules/web-ext/bin/web-ext run --source-dir="../" --firefox=$ffpath --pref marionette.defaultPrefs.enabled=true &
 ext_PID=$!
 
 geckodriver --connect-existing --marionette-port 2828 &
