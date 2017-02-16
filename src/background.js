2af6d191ea46c0360ba6205ebfa27094252805fc
Blake Griffith <cowlicks@riseup.net>
2017-02-15 20:57:49 -0800

    Use new sha1 function

--- src/background.js
+++ src/background.js
@@ -365,11 +365,12 @@ Badger.prototype = {
         callback(successStatus);
         return;
       }
-      var hash = window.SHA1(response); // eslint-disable-line new-cap
-      if(dnt_hashes.hasItem(hash)){
-        successStatus = true;
-      }
-      callback(successStatus);
+      utils.sha1(response, function(hash) {
+        if(dnt_hashes.hasItem(hash)){
+          successStatus = true;
+        }
+        callback(successStatus);
+      });
     });
   },
 
