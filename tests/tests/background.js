375033a5158eeb1cf320824e63a0477933b08568
Alexei <alexeiatyahoodotcom@gmail.com>
2017-03-14 14:26:07 -0400

    Upgrade QUnit to 2.2.0

--- tests/tests/background.js
+++ tests/tests/background.js
@@ -1,11 +1,16 @@
 /* globals badger:false */
 (function() {
-  module("Background");
-  asyncTest("DNT policy checking works", function(){
-    expect(1);
-    badger.checkForDNTPolicy('eff.org', 0, function(successStatus) {
-      ok(successStatus, "eff.org has a good DNT policy");
-      start();
+
+  QUnit.module("Background");
+
+  QUnit.test("DNT policy checking works", (assert) => {
+    let done = assert.async();
+    assert.expect(1);
+
+    badger.checkForDNTPolicy('eff.org', 0, function (successStatus) {
+      assert.ok(successStatus, "eff.org has a good DNT policy");
+      done();
     });
   });
-})();
+
+}());
--- tests/tests/storage.js
+++ tests/tests/storage.js
@@ -2,9 +2,9 @@
 (function() {
   let BACKUP = {};
 
-  module("Storage", {
-    // note: setup is not module-level, called before every test
-    setup: () => {
+  QUnit.module("Storage", {
+    // called before every test
+    beforeEach: () => {
       // back up settings and heuristic learning
       ['action_map', 'settings_map', 'snitch_map'].forEach(item => {
         let obj = badger.storage.getBadgerStorageObject(item);
@@ -13,7 +13,7 @@
     },
 
     // called after every test
-    teardown: () => {
+    afterEach: () => {
       // restore original settings and heuristic learning
       ['action_map', 'settings_map', 'snitch_map'].forEach(item => {
         let obj = badger.storage.getBadgerStorageObject(item);
@@ -22,48 +22,44 @@
     }
   });
 
-  test("testGetBadgerStorage", function(){
-    expect(1);
+  QUnit.test("testGetBadgerStorage", function (assert) {
     var action_map = badger.storage.getBadgerStorageObject('action_map');
-    ok(action_map.updateObject instanceof Function, "action_map is a pbstorage");
+    assert.ok(action_map.updateObject instanceof Function, "action_map is a pbstorage");
   });
 
-  test("test BadgerStorage methods", function(){
-    expect(3);
+  QUnit.test("test BadgerStorage methods", function (assert) {
     var action_map = badger.storage.getBadgerStorageObject('action_map');
     action_map.setItem('foo', 'bar');
-    ok(action_map.getItem('foo') === 'bar');
-    ok(action_map.hasItem('foo'));
+    assert.ok(action_map.getItem('foo') === 'bar');
+    assert.ok(action_map.hasItem('foo'));
     action_map.deleteItem('foo');
-    ok(!action_map.hasItem('foo'));
+    assert.ok(!action_map.hasItem('foo'));
   });
 
-  test("test user override of default action for domain", function(){
-    expect(4);
+  QUnit.test("test user override of default action for domain", function (assert) {
     badger.saveAction("allow", "pbtest.org");
-    ok(badger.userAllow.indexOf('pbtest.org') > -1);
+    assert.ok(badger.userAllow.indexOf('pbtest.org') > -1);
     badger.saveAction("block", "pbtest.org");
-    ok(badger.userAllow.indexOf('pbtest.org') <= -1);
+    assert.ok(badger.userAllow.indexOf('pbtest.org') <= -1);
     badger.saveAction("allow", "pbtest.org");
-    ok(badger.userAllow.indexOf('pbtest.org') > -1);
+    assert.ok(badger.userAllow.indexOf('pbtest.org') > -1);
     badger.storage.revertUserAction("pbtest.org");
-    ok(badger.userAllow.indexOf('pbtest.org') <= -1);
+    assert.ok(badger.userAllow.indexOf('pbtest.org') <= -1);
   });
 
-  test("data persists to local storage", function(){
-    // TODO: Figure out how to test this.
-    expect(1); //expect 1 assertion
-    /*var action_map = BadgerStore.getBadgerStorageObject('action_map');
+  // TODO: Figure out how to test this.
+  QUnit.skip("data persists to local storage", function (/*assert*/) {
+    /*let done = assert.async();
+    var action_map = BadgerStore.getBadgerStorageObject('action_map');
     action_map.setItem('foo', 'bar');
     setTimeout(function(){
       var data = JSON.parse(localStorage.getItem('action_map'));
-      ok(data.foo == 'bar', "data persists to local storage");
-      start();
+      assert.ok(data.foo == 'bar', "data persists to local storage");
+      done();
     }, 500);*/
-    ok(true);
   });
 
-  test("settings map merging", () => {
+  QUnit.test("settings map merging", (assert) => {
     let settings_map = badger.storage.getBadgerStorageObject('settings_map');
 
     // overwrite settings with test values
@@ -77,47 +73,45 @@
     });
 
     // verify
-    deepEqual(
+    assert.deepEqual(
      settings_map.getItem('disabledSites'),
      ['example.com', 'www.nytimes.com'],
       "disabled site lists are combined when merging settings"
     );
-    ok(!settings_map.getItem('showCounter'), "other settings are overwritten");
+    assert.ok(!settings_map.getItem('showCounter'), "other settings are overwritten");
   });
 
-  test("action map merging", () => {
-    expect(2);
+  QUnit.test("action map merging", (assert) => {
     let action_map = badger.storage.getBadgerStorageObject('action_map');
 
     action_map.setItem('testsite.com',
         {dnt: false, heuristicAction: "", nextUpdateTime: 100, userAction: ""});
-    ok(action_map.getItem('testsite.com').nextUpdateTime === 100);
+    assert.ok(action_map.getItem('testsite.com').nextUpdateTime === 100);
 
     let newValue = {dnt: false, heuristicAction: "", nextUpdateTime: 101, userAction: ""};
     action_map.merge({'testsite.com': newValue});
 
     // Merged in object should have new nextUpdateTime value
-    ok(action_map.getItem('testsite.com').nextUpdateTime === 101);
+    assert.ok(action_map.getItem('testsite.com').nextUpdateTime === 101);
   });
 
 
-  test("snitch map merging", () => {
-    expect(5);
+  QUnit.test("snitch map merging", (assert) => {
     let snitch_map = badger.storage.getBadgerStorageObject('snitch_map');
     let action_map = badger.storage.getBadgerStorageObject('action_map');
 
     snitch_map.merge({"testsite.com": ['firstparty.org']});
-    ok(snitch_map.getItem('testsite.com').indexOf('firstparty.org') > -1);
+    assert.ok(snitch_map.getItem('testsite.com').indexOf('firstparty.org') > -1);
 
     // Check to make sure existing and new domain are present
     snitch_map.merge({"testsite.com": ['firstparty2.org']});
-    ok(snitch_map.getItem('testsite.com').indexOf('firstparty.org') > -1);
-    ok(snitch_map.getItem('testsite.com').indexOf('firstparty2.org') > -1);
+    assert.ok(snitch_map.getItem('testsite.com').indexOf('firstparty.org') > -1);
+    assert.ok(snitch_map.getItem('testsite.com').indexOf('firstparty2.org') > -1);
 
     // Verify 'block' status is triggered once TRACKING_THRESHOLD is hit
-    ok(action_map.getItem('testsite.com').heuristicAction === "allow");
+    assert.ok(action_map.getItem('testsite.com').heuristicAction === "allow");
     snitch_map.merge({'testsite.com': ["firstparty3.org"]});
-    ok(action_map.getItem('testsite.com').heuristicAction === "block");
+    assert.ok(action_map.getItem('testsite.com').heuristicAction === "block");
   });
 
 })();
