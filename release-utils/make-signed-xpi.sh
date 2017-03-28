0d114f39423c413ddedaec9159912602b0eb4433
Alexei <alexeiatyahoodotcom@gmail.com>
2017-03-28 13:10:39 -0400

    Update Firefox release script for new sources dir.

--- release-utils/make-signed-xpi.sh
+++ release-utils/make-signed-xpi.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 set -e
-CURDIR="`dirname $0`"
-cd "`dirname $0`"
+
+cd "$(dirname "$0")"
 
 LATEST_SDK_VERSION=1.6.0
 
@@ -16,31 +17,45 @@ if ! type web-ext > /dev/null; then
 fi
 
 if ! web-ext --version | grep -q "$LATEST_SDK_VERSION"; then
-    echo "Please use the latest stable web-ext version or edit this script to the current version."
-    exit 1
+  echo "Please use the latest stable web-ext version or edit this script to the current version."
+  exit 1
 fi
 
-if [ $# -ne 1 ] ; then
+if [ $# -ne 1 ]; then
   echo "Usage: $0 <version to release>"
   exit 1
 fi
+
+if [ -d ../checkout/dist ]; then
+  # new folder structure: https://github.com/EFForg/privacybadger/pull/1278
+  CHECKOUT=checkout/dist
+else
+  # old folder structure
+  CHECKOUT=checkout
+fi
+
+# TODO restore: https://github.com/EFForg/privacybadger/issues/1158
 echo "remove fingerprinting"
-sed -i '/        "src\/fingerprinting.js",/d' ../checkout/manifest.json
+sed -i '/        "src\/fingerprinting.js",/d' ../$CHECKOUT/manifest.json
 
 echo "change author value"
-sed -i 's/"author": { "email": "eff.software.projects@gmail.com" },/"author": "eff.software.projects@gmail.com",/' ../checkout/manifest.json
+sed -i 's/"author": { "email": "eff.software.projects@gmail.com" },/"author": "eff.software.projects@gmail.com",/' ../$CHECKOUT/manifest.json
 
 echo "making zip file for AMO"
 pushd ../checkout
-zip -r ../pkg/$AMO_ZIP_NAME *
+if [ -d dist ]; then
+  (cd dist && zip -r ../../pkg/"$AMO_ZIP_NAME" ./*)
+else
+  zip -r ../pkg/"$AMO_ZIP_NAME" ./*
+fi
 popd
 
 echo "insert self hosting package id"
 # Insert self hosted package id
-sed -i 's,"id": "jid1-MnnxcxisBPnSXQ@jetpack","id": "jid1-MnnxcxisBPnSXQ-eff@jetpack"\,\n      "update_url": "https://www.eff.org/files/privacy-badger-updates.json",' ../checkout/manifest.json
+sed -i 's,"id": "jid1-MnnxcxisBPnSXQ@jetpack","id": "jid1-MnnxcxisBPnSXQ-eff@jetpack"\,\n      "update_url": "https://www.eff.org/files/privacy-badger-updates.json",' ../$CHECKOUT/manifest.json
 
 #"update_url": "https://www.eff.org/files/privacy-badger-updates.json"
 # Build and sign the XPI 
 echo "Running web-ext sign"
-web-ext sign -s ../checkout --api-key $AMO_API_KEY --api-secret $AMO_API_SECRET -a ../pkg
+web-ext sign -s ../$CHECKOUT --api-key "$AMO_API_KEY" --api-secret "$AMO_API_SECRET" -a ../pkg
 mv "../pkg/$PRE_XPI_NAME" "../pkg/$XPI_NAME"
