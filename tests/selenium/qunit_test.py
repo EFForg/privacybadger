cce99d4d9cec2d8d340dd058de4d01347721c320
Alexei <alexeiatyahoodotcom@gmail.com>
2017-04-13 11:33:49 -0400

    Report failed QUnit tests.
    Fixes https://github.com/EFForg/privacybadger/issues/1262

--- tests/selenium/qunit_test.py
+++ tests/selenium/qunit_test.py
@@ -15,18 +15,26 @@ class Test(pbtest.PBSeleniumTest):
         # Probably related to Chromium bugs 129181 & 132148
         self.load_url(self.bg_url)
         self.load_url(self.test_url)
+
         try:
             failed = self.txt_by_css("#qunit-testresult-display > span.failed",
                                      timeout=120)
         except TimeoutException as exc:
             self.fail("Cannot find the results of QUnit tests %s" % exc)
+
         passed = self.txt_by_css("#qunit-testresult-display > span.passed")
         total = self.txt_by_css("#qunit-testresult-display > span.total")
         print("QUnits tests: Failed: %s Passed: %s Total: %s" % (failed,
                                                                  passed,
                                                                  total))
-        self.assertEqual("0", failed)
-        # TODO: Report failed QUnit tests
+        failed_test_els = self.driver.find_elements_by_css_selector(
+            ".fail .test-name"
+        )
+        fail_msg = "The following QUnit tests failed:\n  * {}".format(
+            "\n  * ".join([el.text for el in failed_test_els])
+        )
+
+        self.assertTrue(failed == "0", msg=fail_msg)
 
 if __name__ == "__main__":
     unittest.main()
