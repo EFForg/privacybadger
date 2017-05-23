bf56cfabbb299f6307e892e445df1067f31dba67
Blake Griffith <cowlicks@riseup.net>
2017-05-23 14:00:01 -0700

    Clean up and add first_parties/twitter.js

--- src/js/first_parties.js
+++ src/js/first_parties.js
@@ -1,68 +1,17 @@
-/*
+var firstPartyScripts = {
+  "twitter.com": "twitter.js"
+};
 
-  <a
-      href="https://t.co/U4YMYoAUCK"
-      rel="nofollow noopener"
-      dir="ltr"
-      data-expanded-url="https://www.supremecourt.gov/opinions/16pdf/16-341_8n59.pdf"
-      class="twitter-timeline-link" target="_blank"
-      title="https://www.supremecourt.gov/opinions/16pdf/16-341_8n59.pdf"
-  >
-      <span class="tco-ellipsis"></span>
-      <span class="invisible">https://www.</span>
-      <span class="js-display-url">supremecourt.gov/opinions/16pdf</span>
-      <span class="invisible">/16-341_8n59.pdf</span>
-      <span class="tco-ellipsis"><span class="invisible">&nbsp;</span>…</span>
-    </a>
-  
-*/
-
-function insertScript(text) {
-  var parent = document.documentElement,
-    script = document.createElement('script');
-
-  script.textContent = text;
-  //script.async = false;
-
-  parent.insertBefore(script, parent.firstChild);
-  //parent.removeChild(script);
+function insert() {
+  let scriptName = firstPartyScripts[window.location.hostname];
+  if (!scriptName) {
+    return;
+  }
+  let script = document.createElement('script');
+  script.src = chrome.extension.getURL('js/first_parties/' + scriptName);
+  script.onload = function() {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(script);
 }
-
-
-var code = '(' + function() {
-  function setConsole() {
-    iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    document.body.appendChild(iframe);
-    console = iframe.contentWindow.console;
-    window.console = console;
-    console.log("got console")
-  }
-
-  var elems = [];
-  function unwrap(elem) { 
-    var url = elem.href;
-    if (url && url.toLowerCase().startsWith("https://t.co/")) {
-      var deu = elem.getAttribute("data-expanded-url");
-      if (deu && deu.startsWith("https://")) {
-        elems.push(elem);
-        elem.href = deu;
-      }
-    }
-  }
-
-  function unwrapTwitterURLs() {
-    setConsole();
-    console.log(elems);
-    var aElems = document.getElementsByTagName("a");
-    var n = aElems.length;
-    for (var i = 0; i < n; i++) {
-      var elem = aElems[i];
-      unwrap(elem);
-    }
-  }
-
-  setTimeout(() => {unwrapTwitterURLs()}, 1000);
-} + ')();';
-
-insertScript(code);
+insert();
