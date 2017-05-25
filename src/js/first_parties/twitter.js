b348e037dbd989e368640658ce8d74a5ff255ee0
Blake Griffith <cowlicks@riseup.net>
2017-05-24 19:37:54 -0700

    Add tweetdeck and noreferrer support.

--- src/js/first_parties/twitter.js
+++ src/js/first_parties/twitter.js
@@ -1,16 +1,29 @@
+const deu = "data-expanded-url",
+  dfu = "data-full-url";
+const twitterQuery = "a[" + deu + "][href^='https://t.co/']",
+  tweetdeckQuery = "a[" + dfu + "][href^='https://t.co/']";
+
+function maybeAddNoreferrer(el) {
+  let rel = el.rel ? el.rel : "";
+  if (!rel.includes("noreferrer")) {rel += " noreferrer";}
+  el.rel = rel;
+}
+
 function unwrapTwitterURLs() {
-  let aElems = document.querySelectorAll("a[data-expanded-url][href^='https://t.co/']");
+  let aElems = document.querySelectorAll(twitterQuery + ", " + tweetdeckQuery);
   let n = aElems.length;
   for (let i = 0; i < n; i++) {
     let elem = aElems[i];
-    let deu = elem.getAttribute("data-expanded-url");
-    if (deu && (deu.startsWith("https://") || deu.startsWith("http://"))) {
-      elem.href = deu;
+    let attr = elem.getAttribute(deu);
+    if (!attr) {attr = elem.getAttribute(dfu);}
+    if (attr && (attr.startsWith("https://") || attr.startsWith("http://"))) {
+      elem.href = attr;
       elem.addEventListener("click", function (e) {
         e.stopPropagation();
       });
+      maybeAddNoreferrer(elem);
     }
   }
-  setTimeout(unwrapTwitterURLs, 1500);
+  setTimeout(() => {unwrapTwitterURLs();}, 1500);
 }
 unwrapTwitterURLs();
