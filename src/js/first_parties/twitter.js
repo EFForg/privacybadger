912eabbb487b47295fee4986edcd525ae7c87f2a
Blake Griffith <cowlicks@riseup.net>
2017-05-28 16:04:33 -0700

    Replace t.co links in cards.

--- src/js/first_parties/twitter.js
+++ src/js/first_parties/twitter.js
@@ -8,17 +8,24 @@ function maybeAddNoreferrer(element) {
 
 function unwrapTwitterURLs() {
   let query = "a[" + config.queryParam + "][href^='https://t.co/'], a[" + config.queryParam + "][href^='http://t.co/']";
+  // get links with t.co url's and destination url
   let aElems = document.querySelectorAll(query);
   let n = aElems.length;
   for (let i = 0; i < n; i++) {
     let element = aElems[i];
     let attr = element.getAttribute(config.queryParam);
     if (attr && (attr.startsWith("https://") || attr.startsWith("http://"))) {
-      element.href = attr;
-      element.addEventListener("click", function (e) {
-        e.stopPropagation();
-      });
-      maybeAddNoreferrer(element);
+      // replace all the t.co url's tha correspond to this destination
+      matchingTcos = document.querySelectorAll("a[href='" + element.href + "']");
+      nMatches = matchingTcos.length;
+      for (let j = 0; j < nMatches; j++) {
+        tco = matchingTcos[j];
+        tco.href = attr;
+        tco.addEventListener("click", function (e) {
+          e.stopPropagation();
+        });
+        maybeAddNoreferrer(tco);
+      }
     }
   }
   setTimeout(() => {unwrapTwitterURLs();}, 2000);
