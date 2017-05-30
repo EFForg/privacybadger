11a2e5176fb06ae4499d167daf5f25cb59fc782f
Blake Griffith <cowlicks@riseup.net>
2017-05-29 23:43:42 -0700

    Use for..of on nodelist, make unwrapTco function.

--- src/js/first_parties/twitter.js
+++ src/js/first_parties/twitter.js
@@ -6,25 +6,25 @@ function maybeAddNoreferrer(element) {
   element.rel = rel;
 }
 
+function unwrapTco(tco, attr) {
+  tco.href = attr;
+  tco.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+  maybeAddNoreferrer(tco);
+}
+
 function unwrapTwitterURLs() {
   let query = "a[" + config.queryParam + "][href^='https://t.co/'], a[" + config.queryParam + "][href^='http://t.co/']";
   // get links with t.co url's and destination url
   let aElems = document.querySelectorAll(query);
-  let n = aElems.length;
-  for (let i = 0; i < n; i++) {
-    let element = aElems[i];
+  for (let element of aElems) {
     let attr = element.getAttribute(config.queryParam);
     if (attr && (attr.startsWith("https://") || attr.startsWith("http://"))) {
       // replace all the t.co url's tha correspond to this destination
       let matchingTcos = document.querySelectorAll("a[href='" + element.href + "']");
-      let nMatches = matchingTcos.length;
-      for (let j = 0; j < nMatches; j++) {
-        let tco = matchingTcos[j];
-        tco.href = attr;
-        tco.addEventListener("click", function (e) {
-          e.stopPropagation();
-        });
-        maybeAddNoreferrer(tco);
+      for (let tco of matchingTcos) {
+        unwrapTco(tco, attr);
       }
     }
   }
