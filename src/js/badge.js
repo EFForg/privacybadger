480dab0e091f126689043dfe5a49c27b377d83a4
Blake Griffith <cowlicks@riseup.net>
2017-06-10 13:17:12 -0700

    refactor badge.getIcons

--- src/js/badge.js
+++ src/js/badge.js
@@ -9,19 +9,14 @@ let sizes = [16, 19, 24, 32, 38, 48, 64, 128, 170];
 function getIcons(enabled) {
   let out = {};
   sizes.forEach((size) => {
-    let filename = 'icons/badger-' + size;
-    if (!enabled) {
-      filename += '-disabled';
-    }
-    filename += '.png';
-
+    let filename = 'icons/badger-' + size + (enabled ? '' : '-disabled') + '.png';
     out[size] =  chrome.runtime.getURL(filename);
   });
   return out;
 }
 
 let enabled_icons = getIcons(true);
-let disabled_icons = getIcons(true);
+let disabled_icons = getIcons(false);
 
 function refresh(tab) {
   let icons = badger.isPrivacyBadgerEnabled(window.extractHostFromURL(tab.url)) ? enabled_icons : disabled_icons;
