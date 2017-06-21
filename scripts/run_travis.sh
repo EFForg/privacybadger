2019a0bb9d59e0e09ad099bea555d4161a9f1a4d
Blake Griffith <cowlicks@riseup.net>
2017-06-21 15:31:33 -0700

    simplify run_travis.sh script

--- scripts/run_travis.sh
+++ scripts/run_travis.sh
@@ -3,50 +3,33 @@ toplevel=$(git rev-parse --show-toplevel)
 testdir=${toplevel}/tests/selenium
 
 function run_lint {
-  pushd $toplevel
-  make lint
+  make -C ${toplevel} lint
   if [ $? != 0 ]; then
     echo "Linting errors"
     exit 1
   fi
-  popd
 }
 
 function run_selenium {
   py.test --capture=no --verbose --durations=10 ${testdir} # autodiscover and run the tests
 }
 
-function set_info {
-  if [ -n "$INFO" ]; then
-    case $BROWSER in
-      *chrome*)
-          echo "setting info=chrome"
-        export INFO=chrome
-        ;;
-      *firefox*)
-          echo "setting info=firefox"
-        export INFO=firefox
-        ;;
-    esac
-  fi
-}
-
-set_info
-case $INFO in
-  *chrome*)
-    echo "running tests on chrome"
-    run_selenium
-    ;;
-  *firefox*)
-    echo "running tests on firefox"
-    run_selenium
-    ;;
-  *lint*)
+if [ "$INFO" == "lint" ]; then
     echo "running lint tests"
     run_lint
-    ;;
-  *)
-    echo "bad INFO variable, got $INFO"
-    exit 1
-    ;;
-esac
+else
+    case $BROWSER in
+      *chrome*)
+        echo "running tests on chrome"
+        run_selenium
+        ;;
+      *firefox*)
+        echo "running tests on firefox"
+        run_selenium
+        ;;
+      *)
+        echo "bad INFO variable, got $INFO"
+        exit 1
+        ;;
+    esac
+fi
