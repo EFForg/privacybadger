1420fc9349bf6b9cc9cf0e83873d3dea2ff4d1bb
Blake Griffith <cowlicks@riseup.net>
2017-06-28 19:09:08 -0700

    Use new listeners

--- src/js/heuristicblocking.js
+++ src/js/heuristicblocking.js
@@ -528,34 +528,16 @@ function startListeners() {
   /**
    * Adds heuristicBlockingAccounting as listened to onBeforeSendHeaders request
    */
-  chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-    if (badger) {
-      return badger.heuristicBlocking.heuristicBlockingAccounting(details);
-    } else {
-      return {};
-    }
-  }, {urls: ["<all_urls>"]}, ["requestHeaders"]);
+  chrome.webRequest.onBeforeSendHeaders.addListener(
+    onBeforeSendHeaders,
+    {urls: ["<all_urls>"]}, ["requestHeaders"]);
 
   /**
    * Adds onResponseStarted listener. Monitor for cookies
    */
-  chrome.webRequest.onResponseStarted.addListener(function(details) {
-    var hasSetCookie = false;
-    for(var i = 0; i < details.responseHeaders.length; i++) {
-      if(details.responseHeaders[i].name.toLowerCase() == "set-cookie") {
-        hasSetCookie = true;
-        break;
-      }
-    }
-    if(hasSetCookie) {
-      if (badger) {
-        return badger.heuristicBlocking.heuristicBlockingAccounting(details);
-      } else {
-        return {};
-      }
-    }
-  },
-  {urls: ["<all_urls>"]}, ["responseHeaders"]);
+  chrome.webRequest.onResponseStarted.addListener(
+    onBeforeResponseStarted,
+    {urls: ["<all_urls>"]}, ["responseHeaders"]);
 }
 
 /************************************** exports */
