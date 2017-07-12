74c6165fd5b7ae5fd07fc74c3c47bafc5913e38d
Blake Griffith <cowlicks@riseup.net>
2017-07-12 15:17:36 -0700

    Add messages.js

---
+++ src/js/messages.js
@@ -0,0 +1,35 @@
+require.scopes.messages = (() => {
+
+let methods = new Set([
+  'isPrivacyBadgerEnabled',
+]);
+
+function Listener(badger) {
+  this.badger = badger;
+  this.methods = methods;
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (methods.has(request.method)) {
+        sendResponse(badger[request.method](badger, request.args));
+      }
+    }
+  );
+}
+
+function Client() {
+  function sendMessage(method, args, callback) {
+    chrome.runtime.sendMessage({'method': method, 'args': args}, callback);
+  };
+
+  this.isPrivacyBadgerEnabled = function(origin, callback) {
+    sendMessage('isPrivacyBadgerEnabled', [origin], callback);
+  };
+}
+
+let exports = {
+  Listener: Listener,
+  Client: Client,
+};
+
+return exports;
+})();
