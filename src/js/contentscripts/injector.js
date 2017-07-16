33d45d0b8b99155e022bf7c787af808f4b5e494d
Blake Griffith <cowlicks@riseup.net>
2017-07-16 13:25:46 -0700

    Add listening to finger-counting code injector.

--- src/js/contentscripts/injector.js
+++ src/js/contentscripts/injector.js
@@ -1,6 +1,14 @@
-var s = document.createElement('script');
+let event_id = Math.random();
+
+// listen for messages from the script we are about to insert
+document.addEventListener(event_id, function (e) {
+  chrome.runtime.sendMessage(e.detail);
+});
+
+let s = document.createElement('script');
+s.setAttribute('data', event_id);
 s.src = chrome.extension.getURL('js/injected/fingercounting.js');
 s.onload = function() {
-    this.remove();
+  this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
