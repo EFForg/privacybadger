1f1eef43297e79d5477eed3d9d84b82f49d6533b
Blake Griffith <cowlicks@riseup.net>
2017-07-24 00:35:29 -0700

    Remove redundant try/except statements.

--- tests/selenium/popup_test.py
+++ tests/selenium/popup_test.py
@@ -20,10 +20,7 @@ class PopupTest(pbtest.PBSeleniumTest):
         self.load_url(self.popup_url, wait_on_site=1)
         if close_overlay:
             # Click 'X' element to close overlay.
-            try:
-                close_element = self.driver.find_element_by_id("fittslaw")
-            except NoSuchElementException:
-                self.fail("Unable to find element to close popup overlay")
+            close_element = self.driver.find_element_by_id("fittslaw")
             close_element.click()
 
             # Element will fade out so wait for it to disappear.
@@ -31,27 +28,17 @@ class PopupTest(pbtest.PBSeleniumTest):
 
     def get_enable_button(self):
         """Get enable button on popup."""
-        try:
-            return self.driver.find_element_by_id("activate_site_btn")
-        except NoSuchElementException:
-            self.fail("Unable to find enable button on poup")
+        return self.driver.find_element_by_id("activate_site_btn")
 
     def get_disable_button(self):
         """Get disable button on popup."""
-        try:
-            return self.driver.find_element_by_id("deactivate_site_btn")
-        except NoSuchElementException:
-            self.fail("Unable to find disable buttons on popup")
+        return self.driver.find_element_by_id("deactivate_site_btn")
 
     def test_overlay(self):
         """Ensure overlay links to first run comic."""
         self.open_popup(close_overlay=False)
 
-        try:
-            comic_link = self.driver.find_element_by_id("firstRun")
-        except NoSuchElementException:
-            self.fail("Unable to find link to comic on popup overlay")
-        comic_link.click()
+        self.driver.find_element_by_id("firstRun").click()
 
         # Make sure first run comic not opened in same window.
         time.sleep(1)
@@ -70,11 +57,7 @@ class PopupTest(pbtest.PBSeleniumTest):
         """Ensure first run page is opened when help button is clicked."""
         self.open_popup()
 
-        try:
-            help_button = self.driver.find_element_by_id("help")
-        except NoSuchElementException:
-            self.fail("Unable to find help button on popup")
-        help_button.click()
+        self.driver.find_element_by_id("help").click()
 
         # Make sure first run page not opened in same window.
         time.sleep(1)
@@ -93,11 +76,7 @@ class PopupTest(pbtest.PBSeleniumTest):
         """Ensure options page is opened when button is clicked."""
         self.open_popup()
 
-        try:
-            options_button = self.driver.find_element_by_id("options")
-        except NoSuchElementException:
-            self.fail("Unable to find options button on popup")
-        options_button.click()
+        self.driver.find_element_by_id("options").click()
 
         # Make sure options page not opened in same window.
         time.sleep(1)
@@ -116,11 +95,7 @@ class PopupTest(pbtest.PBSeleniumTest):
         """Ensure trackers link opens EFF website."""
         self.open_popup()
 
-        try:
-            trackers_link = self.driver.find_element_by_link_text("trackers")
-        except NoSuchElementException:
-            self.fail("Unable to find trackers link on popup")
-
+        trackers_link = self.driver.find_element_by_link_text("trackers")
         handles_before = set(self.driver.window_handles)
         trackers_link.click()
 
@@ -200,12 +175,8 @@ class PopupTest(pbtest.PBSeleniumTest):
         """Ensure donate button opens EFF website."""
         self.open_popup()
 
-        try:
-            donate_button = self.driver.find_element_by_id("donate")
-        except NoSuchElementException:
-            self.fail("Unable to find donate button on popup")
+        donate_button = self.driver.find_element_by_id("donate")
         handles_before = set(self.driver.window_handles)
-
         donate_button.click()
 
         # Make sure EFF website not opened in same window.
