681eb0d836d1f31d03eb05a7e4a9e248ca4578f3
Blake Griffith <cowlicks@riseup.net>
2017-08-02 12:44:12 -0700

    Test client more thoroughly.

--- src/tests/tests/messages.js
+++ src/tests/tests/messages.js
@@ -1,42 +1,64 @@
 (function() {
-  let messages = require('messages');
-  QUnit.module("Messages");
+  let messages = require('messages'),
+    methods = new Set([
+      'base.method',
+      'base.another.thing',
+      'base.even.longer.stuff',
+    ]),
+    beforeSendMessage = chrome.runtime.sendMessage;
+
+  QUnit.module("Messages", {
+    before: () => {
+      // dummy sendMessage func
+      chrome.runtime.sendMessage = function (messageObject, responseCallback) {
+        responseCallback(messageObject);
+      };
+    },
+    after: () => {
+      chrome.runtime.sendMessage = beforeSendMessage;
+    },
+  });
+
   QUnit.test('Client test', (assert) => {
     let done = assert.async(2);
-    assert.expect(9);
+    assert.expect(12);
 
 
-    let client = new messages.Client(
-      new Set([
-        'base.method',
-        'base.another.thing',
-        'base.even.longer.stuff',
-      ])
-    );
+    let client = new messages.Client(methods);
+
+    // assert methods are added to client
     assert.ok('method' in client);
     assert.ok('another' in client);
     assert.ok('stuff' in client.even.longer);
 
-    let beforeSendMessage = chrome.runtime.sendMessage;
-    chrome.runtime.sendMessage = function (obj, func) {
-      func(obj);
-    };
-
-    client.method('foo', 6).then(obj => {
+    client.method('foo', 'bar').then(obj => {
       assert.ok(obj.method === 'base.method');
       assert.ok(obj.args[0] == 'foo');
-      assert.ok(obj.args[1] == 6);
+      assert.ok(obj.args[1] == 'bar');
+      assert.ok(obj.args.length == 2);
+
+      done();
+    });
+
+    client.another.thing(1, 2, 3).then(obj => {
+      assert.ok(obj.method === 'base.another.thing');
+      assert.ok(obj.args[0] == 1);
+      assert.ok(obj.args[1] == 2);
+      assert.ok(obj.args[2] == 3);
+      assert.ok(obj.args.length == 3);
 
       done();
     });
 
     // test callback
-    client.even.longer.stuff(res => {
-      res['callbackCalled'] = true;
+    client.even.longer.stuff(1, 2, res => {
+      res.callbackCalled = true;
       return res;
     }).then(obj => {
       assert.ok(obj.method === 'base.even.longer.stuff');
-      assert.ok(obj.args.length == 0);
+      assert.ok(obj.args[0] == 1);
+      assert.ok(obj.args[1] == 2);
+      assert.ok(obj.args.length == 2);
       assert.ok(obj.callbackCalled);
 
       done();
