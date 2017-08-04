3d9a6d62d80d2af60c5dd6ce7599b8c7ce71f8d0
Alexei <alexeiatyahoodotcom@gmail.com>
2017-08-04 15:19:05 -0400

    Add a couple of tests, documentation; tweak style.

--- src/tests/tests/multiDomainFirstParties.js
+++ src/tests/tests/multiDomainFirstParties.js
@@ -4,12 +4,40 @@
   let mdfp = require('multiDomainFP');
 
   QUnit.test('isMultiDomainFirstParty test', function (assert) {
-    let testData = [['foo.bar', 'yep.com', 'maybe.idk'], ['related.com', 'larry.com']],
-      isMdfp = mdfp.makeIsMultiDomainFirstParty(mdfp.makeDomainLookup(testData));
+    let testData = [
+      ['foo.bar', 'yep.com', 'maybe.idk'],
+      ['related.com', 'larry.com'],
+    ];
 
-    assert.ok(isMdfp('yep.com', 'maybe.idk'));
-    assert.notOk(isMdfp('yep.com', 'google.com'));
-    assert.notOk(isMdfp('reddit.com', 'eff.org'));
-    assert.notOk(isMdfp('larry.com', 'yep.com'));
+    let isMdfp = mdfp.makeIsMultiDomainFirstParty(mdfp.makeDomainLookup(testData));
+
+    assert.ok(
+      isMdfp('yep.com', 'maybe.idk'),
+      "these are related domains according to test data"
+    );
+    assert.ok(
+      isMdfp('maybe.idk', 'yep.com'),
+      "the domains are related regardless of ordering"
+    );
+    assert.ok(
+      isMdfp('related.com', 'larry.com'),
+      "these should also be related domains, from a different set in test data"
+    );
+    assert.notOk(
+      isMdfp('yep.com', 'related.com'),
+      "these domains are both present in test data but should not be related"
+    );
+    assert.notOk(
+      isMdfp('larry.com', 'yep.com'),
+      "these domains are also both present but should be unrelated"
+    );
+    assert.notOk(
+      isMdfp('yep.com', 'google.com'),
+      "one of these domains is not in test data"
+    );
+    assert.notOk(
+      isMdfp('reddit.com', 'eff.org'),
+      "both domains are not in test data"
+    );
   });
 })();
