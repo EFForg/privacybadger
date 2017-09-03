50d87c8de28bc83eca3f6f2c6eb09334cf7c0f57
eenblam <ben.a.elam@gmail.com>
2017-09-02 19:20:32 -0700

    Strip sample_ from scripts/legacy-cookies.sh

--- scripts/legacy-cookies.sh
+++ scripts/legacy-cookies.sh
@@ -1,1 +1,1 @@
-sed -e "s/^\([^\!].*\)$/@@||\1^\$third-party/g" src/data/sample_cookieblocklist.txt > doc/sample_cookieblocklist_legacy.txt
+sed -e "s/^\([^\!].*\)$/@@||\1^\$third-party/g" src/data/cookieblocklist.txt > doc/sample_cookieblocklist_legacy.txt
