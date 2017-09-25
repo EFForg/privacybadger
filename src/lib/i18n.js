5491fb3f59e93a5f7c898988a9359532e6819cb7
Alexei <alexeiatyahoodotcom@gmail.com>
2017-09-25 14:26:00 -0400

    Replace certain attrs as well as span els for i18n

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -20,11 +20,10 @@ var i18n = chrome.i18n;
 // Loads and inserts i18n strings into matching elements. Any inner HTML already in the
 // element is parsed as JSON and used as parameters to substitute into placeholders in the
 // i18n message.
-function loadI18nStrings()
-{
-  var nodes = document.querySelectorAll("[class^='i18n_']");
-  for(var i = 0; i < nodes.length; i++)
-  {
+function loadI18nStrings() {
+  // replace span contents by their class names
+  let nodes = document.querySelectorAll("[class^='i18n_']");
+  for (let i = 0; i < nodes.length; i++) {
     var arguments = JSON.parse("[" + nodes[i].textContent + "]");
     var className = nodes[i].className;
     if (className instanceof SVGAnimatedString)
@@ -35,6 +34,37 @@ function loadI18nStrings()
       nodes[i][prop] = i18n.getMessage(stringName, arguments);
     else
       nodes[i][prop] = i18n.getMessage(stringName);
+  }
+
+  // also replace title and placeholder attributes
+  const ATTRS = [
+    'placeholder',
+    'title',
+  ];
+
+  // get all the elements that contain one or more of these attributes
+  nodes = document.querySelectorAll(
+    // for example: "[placeholder^='i18n_'], [title^='i18n_']"
+    "[" + ATTRS.join("^='i18n_'], [") + "^='i18n_']"
+  );
+
+  // for each element
+  for (let i = 0; i < nodes.length; i++) {
+    // for each attribute
+    ATTRS.forEach(attr_type => {
+      // get the translation message key
+      let key = nodes[i].getAttribute(attr_type);
+      if (key) {
+        // remove the i18n_ prefix
+        key = key.slice(5);
+      }
+
+      // if the attribute exists and looks like i18n_KEY
+      if (key) {
+        // update the attribute with the result of a translation lookup by KEY
+        nodes[i].setAttribute(attr_type, i18n.getMessage(key));
+      }
+    });
   }
 }
 
