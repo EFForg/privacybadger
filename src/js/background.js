66f7b784c590e7d2dfa51dd74e367e7db9d9fbdf
Alexei <alexeiatyahoodotcom@gmail.com>
2017-09-25 17:41:46 -0400

    Update checkForDNTPolicy's documentation.

--- src/js/background.js
+++ src/js/background.js
@@ -366,18 +366,16 @@ Badger.prototype = {
   * Checks a domain for the EFF DNT policy.
   *
   * @param {String} domain The domain to check
-  * @param {timestamp} nextUpdate Time when the DNT policy should be rechecked
   * @param {Function} cb Callback that receives check status boolean (optional)
   */
   checkForDNTPolicy: function (domain, cb) {
     var badger = this,
-      nextUpdate = badger.storage.getNextUpdateForDomain(domain);
+      next_update = badger.storage.getNextUpdateForDomain(domain);
 
-    if (Date.now() < nextUpdate) {
+    if (Date.now() < next_update) {
       // not yet time
       return;
     }
-
 
     if (! badger.isCheckingDNTPolicyEnabled()) {
       // user has disabled this check
