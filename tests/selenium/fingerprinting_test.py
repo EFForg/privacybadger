ba71e280570600f0fc5966673ffa35b5b3992811
Alexei <alexeiatyahoodotcom@gmail.com>
2017-10-02 18:53:49 -0400

    Work around sporadic fingerprinting test failures.
    On Firefox 56 and 57 ... maybe not ESR (52).

--- tests/selenium/fingerprinting_test.py
+++ tests/selenium/fingerprinting_test.py
@@ -5,6 +5,8 @@ import unittest
 
 import pbtest
 
+from time import sleep
+
 from window_utils import switch_to_window_with_url
 
 
@@ -55,10 +57,16 @@ return (
         switch_to_window_with_url(self.driver, self.bg_url)
 
         # check that we detected the fingerprinting domain as a tracker
-        self.assertTrue(
-            self.detected_tracking(FINGERPRINTING_DOMAIN, PAGE_URL),
-            "Canvas fingerprinting resource was detected as a tracker."
-        )
+        has_tracking = self.detected_tracking(FINGERPRINTING_DOMAIN, PAGE_URL)
+
+        # work around sporadic failures on some versions of Firefox
+        if pbtest.shim.browser_type == 'firefox' and not has_tracking:
+            print("\nRetrying canvas fingerprinting detection check ...")
+            sleep(2)
+            has_tracking = self.detected_tracking(FINGERPRINTING_DOMAIN, PAGE_URL)
+
+        self.assertTrue(has_tracking,
+            "Canvas fingerprinting resource was detected as a tracker.")
 
         # check that we detected canvas fingerprinting
         self.assertTrue(
