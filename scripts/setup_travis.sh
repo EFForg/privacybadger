83a95542a9ca89dd4bef11e914d574f24dbd269d
Alexei <alexeiatyahoodotcom@gmail.com>
2017-11-02 09:36:10 -0400

    Fix lint nits.

--- scripts/setup_travis.sh
+++ scripts/setup_travis.sh
@@ -5,7 +5,7 @@ function setup_chrome {
     # Install the latest version of the chromedriver
     version=$(wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE -q -O -)
     url="https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip"
-    wget -O /tmp/chromedriver.zip ${url}
+    wget -O /tmp/chromedriver.zip "$url"
     sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
     sudo chmod a+x /usr/local/bin/chromedriver
 }
@@ -20,18 +20,18 @@ function setup_firefox {
       version=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest | grep tag_name | cut -d '"' -f 4)
     fi
     url="https://github.com/mozilla/geckodriver/releases/download/${version}/geckodriver-${version}-linux64.tar.gz"
-    wget -O /tmp/geckodriver.tar.gz ${url}
+    wget -O /tmp/geckodriver.tar.gz "$url"
     sudo tar -xvf /tmp/geckodriver.tar.gz -C /usr/local/bin/
     sudo chmod a+x /usr/local/bin/geckodriver
 }
 
 function browser_setup {
   # install python stuff
-  pip install -r ${toplevel}/tests/requirements.txt
+  pip install -r "$toplevel"/tests/requirements.txt
 }
 
 function setup_lint {
-  pushd ${toplevel}
+  pushd "$toplevel"
     npm install
   popd
 
