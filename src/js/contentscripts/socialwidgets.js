6b4eec5cd42fb6650d6ceee4a17b32678383fb3b
Alexei <alexeiatyahoodotcom@gmail.com>
2017-11-14 19:49:46 -0500

    Remove unnecessary "globals" linter directives.

--- src/js/contentscripts/socialwidgets.js
+++ src/js/contentscripts/socialwidgets.js
@@ -42,7 +42,6 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-/* globals chrome, document, window, console*/
 
 /**
  * Social widget tracker data, read from file.
