a7d596ff9a3c21d226fcf1521d46062c2fd45a91
Schatzkin <schatzkin@eff.org>
2018-03-28 15:48:19 -0700

    adding setSeenComic to pb.js

--- src/skin/js/pb.js
+++ src/skin/js/pb.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
+(function($) {
+
+$(window).on("load", function () {
+
+  function setSeenComic() {
+    var badger = chrome.extension.getBackgroundPage().badger;
+    var settings = badger.storage.getBadgerStorageObject("settings_map");
+    settings.setItem("seenComic", true);
+  }
+  setSeenComic;
   $(".scroll-it").smoothScroll();
 });
+
+})(jQuery);
