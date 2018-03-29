2653f20fa16cb2bef34481e085252bbf4fdd4c72
Schatzkin <schatzkin@eff.org>
2018-03-29 10:51:14 -0700

    removing spaces per travis errors

--- src/skin/js/firstRun.js
+++ src/skin/js/firstRun.js
@@ -1,19 +1,19 @@
 (function($) {
 
-  $(window).on("load", function () {
+$(window).on("load", function () {
 
-    function setSeenComic() {
-      var badger = chrome.extension.getBackgroundPage().badger;
-      var settings = badger.storage.getBadgerStorageObject("settings_map");
-      settings.setItem("seenComic", true);
+  function setSeenComic() {
+    var badger = chrome.extension.getBackgroundPage().badger;
+    var settings = badger.storage.getBadgerStorageObject("settings_map");
+    settings.setItem("seenComic", true);
+  }
+
+  $(".scroll-it").smoothScroll({
+    afterScroll: function () {
+      setSeenComic();
     }
-
-    $(".scroll-it").smoothScroll({
-      afterScroll: function () {
-        setSeenComic();
-      }
-    });
-
   });
+
+});
 
 })(jQuery);
