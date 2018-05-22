3b6b685fc803c17541c1d40a43a2f360169e8672
rugk <rugk@posteo.de>
2018-05-22 12:47:01 +0200

    HAndle profile URL as a special case

--- src/js/firstparties/twitter.js
+++ src/js/firstparties/twitter.js
@@ -1,11 +1,26 @@
 let query_param = 'data-expanded-url';
-let tcos_with_destination = "a[" + query_param + "][href^='https://t.co/'], a[" + query_param + "][href^='http://t.co/']";
+let tcos_with_destination = getTCoSelectorWithDestination(query_param);
+let query_param_profile = "title";
+let profile_links_tcos = ".ProfileHeaderCard " + getTCoSelectorWithDestination(query_param_profile);
 let fixes = {};
+
+function getTCoSelectorWithDestination(attribute) {
+  return "a[" + attribute + "][href^='https://t.co/'], a[" + attribute + "][href^='http://t.co/']";
+}
 
 function maybeAddNoreferrer(link) {
   let rel = link.rel ? link.rel : "";
   if (!rel.includes("noreferrer")) {rel += " noreferrer";}
   link.rel = rel;
+}
+
+function getLinkAttribute(node, attribute) {
+  let attr = node.getAttribute(attribute);
+  if (attr && (attr.startsWith("https://") || attr.startsWith("http://"))) {
+    return attr;
+  }
+
+  return null;
 }
 
 function unwrapTco(tco, destination) {
@@ -36,14 +51,18 @@ function findInAllFrames(query) {
   return out;
 }
 
-function unwrapTwitterURLs() {
+function unwrapTwitterURLsInTimeline() {
+  // first iteration is needed to collect all links
   findInAllFrames(tcos_with_destination).forEach((link) => {
-    let attr = link.getAttribute(query_param);
-    if (attr && (attr.startsWith("https://") || attr.startsWith("http://"))) {
+    let attr = getLinkAttribute(link, query_param);
+    if (attr !== null) {
       fixes[link.href] = attr;
+      // once we are here, we can also unwrap it directly, instead of iterating again later
       unwrapTco(link, attr);
     }
   });
+
+  // iterate another time to actually replace the link (of Twitter cards, e.g.)
   findInAllFrames("a[href^='https://t.co/'], a[href^='http://t.co/'").forEach((link) => {
     if (fixes.hasOwnProperty(link.href)) {
       unwrapTco(link, fixes[link.href]);
@@ -51,5 +70,16 @@ function unwrapTwitterURLs() {
   });
 }
 
-unwrapTwitterURLs();
-setInterval(unwrapTwitterURLs, 2000);
+function unwrapSpecialTwitterURLs() {
+  // unwrap profile links
+  document.querySelectorAll(profile_links_tcos).forEach((link) => {
+    let attr = getLinkAttribute(link, query_param_profile);
+    if (attr !== null) {
+      unwrapTco(link, attr);
+    }
+  });
+}
+
+unwrapSpecialTwitterURLs();
+unwrapTwitterURLsInTimeline();
+setInterval(unwrapTwitterURLsInTimeline, 2000);
