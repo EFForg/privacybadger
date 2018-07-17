001efba0d3aa8580249f280294e5d5c17e40c074
Bennett Cyphers <bennett@eff.org>
2018-07-17 14:25:11 -0700

    Remove URL validation regex
    Only check whether a URL starts with http(s), not whether it's a valid
    URL.

--- src/js/firstparties/facebook.js
+++ src/js/firstparties/facebook.js
@@ -34,8 +34,8 @@ function cleanAttrs(elem) {
 function cleanLink(a) {
   let href = new URL(a.href).searchParams.get('u');
 
-  // ensure the URL is actually a URL (either HTTP or HTTPS)
-  let url_regex = new RegExp(/^https?:\/\/([-a-zA-Z0-9]+\.)+[-a-zA-Z0-9]+\b(\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?/g);
+  // ensure the URL starts with HTTP or HTTPS
+  let url_regex = new RegExp(/^https?:\/\//g);
   if (!href || !href.match(url_regex)) {
     // If we can't extract a good URL, abort without breaking the links
     return;
