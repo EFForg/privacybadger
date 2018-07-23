56462e119b0f857891ec5d1fca2c94db44a91b76
Alexei <alexeiatyahoodotcom@gmail.com>
2018-07-23 17:49:32 -0400

    Remove unused document.location lookup.

--- src/js/contentscripts/socialwidgets.js
+++ src/js/contentscripts/socialwidgets.js
@@ -302,7 +302,7 @@ function replaceIndividualButton(tracker) {
 *                            replaced
 */
 function getTrackerData(callback) {
-  chrome.runtime.sendMessage({checkReplaceButton:document.location.hostname}, function(response) {
+  chrome.runtime.sendMessage({checkReplaceButton: true}, function(response) {
     if (response) {
       var trackers = response.trackers;
       var trackerButtonsToReplace = response.trackerButtonsToReplace;
