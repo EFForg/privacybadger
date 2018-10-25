470ce5f3d12a8d9939162c489d18a02810aa4dcb
Alexei <alexeiatyahoodotcom@gmail.com>
2018-10-25 17:49:31 -0400

    Make clobbering checks more specific.
    To ensure we match clobbered localStorage APIs, check for null/undefined
    instead of just empty string ("no result").
    
    Also verify we get back expected values from localStorage before
    overwriting.

--- tests/selenium/clobbering_test.py
+++ tests/selenium/clobbering_test.py
@@ -8,16 +8,17 @@ import pbtest
 
 class ClobberingTest(pbtest.PBSeleniumTest):
     def test_localstorage_clobbering(self):
-        FIXTURE_TEST_RESULT_IDS = [
-            'get-item',
-            'get-property',
-            'get-item-proto',
+        LOCALSTORAGE_TESTS = [
+            # (test result element ID, expected stored, expected empty)
+            ('get-item', "qwerty", "null"),
+            ('get-property', "asdf", "undefined"),
+            ('get-item-proto', "qwerty", "null"),
         ]
         # page loads a frame that writes to and reads from localStorage
         # TODO remove delays from fixture once race condition (https://crbug.com/478183) is fixed
         FIXTURE_URL = (
             "https://gitcdn.link/cdn/ghostwords/"
-            "95d3795b3e2d59b0a729825050c252d2/raw/f357323dba57eaae2fadd89ae8ad7644f47f621d/"
+            "95d3795b3e2d59b0a729825050c252d2/raw/a1017b1b0e991dc4a2f2903afb221b1f76da2300/"
             "privacy-badger-clobbering-fixture.html"
         )
         FRAME_DOMAIN = "githack.com"
@@ -28,32 +29,7 @@ class ClobberingTest(pbtest.PBSeleniumTest):
         # first allow localStorage to be set
         self.load_url(FIXTURE_URL)
         self.wait_for_and_switch_to_frame('iframe')
-        for selector in FIXTURE_TEST_RESULT_IDS:
-            # wait for each test to run
-            self.wait_for_script(
-                "return document.getElementById('%s')"
-                ".textContent != '...';" % selector,
-                timeout=2,
-                message=(
-                    "Timed out waiting for localStorage (%s) to finish ... "
-                    "This probably means the fixture "
-                    "errored out somewhere." % selector
-                )
-            )
-            self.assertNotEqual(
-                self.txt_by_css("#" + selector), "",
-                "localStorage (%s) was not read successfully"
-                "for some reason" % selector
-            )
-
-        # mark the frame domain for cookieblocking
-        self.load_url(self.options_url)
-        self.js(COOKIEBLOCK_JS)
-
-        # now rerun and check results for various localStorage access tests
-        self.load_url(FIXTURE_URL)
-        self.wait_for_and_switch_to_frame('iframe')
-        for selector in FIXTURE_TEST_RESULT_IDS:
+        for selector, expected, _ in LOCALSTORAGE_TESTS:
             # wait for each test to run
             self.wait_for_script(
                 "return document.getElementById('%s')"
@@ -66,7 +42,32 @@ class ClobberingTest(pbtest.PBSeleniumTest):
                 )
             )
             self.assertEqual(
-                self.txt_by_css("#" + selector), "",
+                self.txt_by_css("#" + selector), expected,
+                "localStorage (%s) was not read successfully"
+                "for some reason" % selector
+            )
+
+        # mark the frame domain for cookieblocking
+        self.load_url(self.options_url)
+        self.js(COOKIEBLOCK_JS)
+
+        # now rerun and check results for various localStorage access tests
+        self.load_url(FIXTURE_URL)
+        self.wait_for_and_switch_to_frame('iframe')
+        for selector, _, expected in LOCALSTORAGE_TESTS:
+            # wait for each test to run
+            self.wait_for_script(
+                "return document.getElementById('%s')"
+                ".textContent != '...';" % selector,
+                timeout=2,
+                message=(
+                    "Timed out waiting for localStorage (%s) to finish ... "
+                    "This probably means the fixture "
+                    "errored out somewhere." % selector
+                )
+            )
+            self.assertEqual(
+                self.txt_by_css("#" + selector), expected,
                 "localStorage (%s) was read despite cookieblocking" % selector
             )
 
