ac5886fc52f665a604fcdc8dd918b2c7d516c3a1
Elias Ojala <git@eliasojala.me>
2018-12-29 17:17:46 +0200

    Ubisoft MDFP

--- src/js/multiDomainFirstParties.js
+++ src/js/multiDomainFirstParties.js
@@ -1419,6 +1419,7 @@ var multiDomainFirstPartiesArray = [
   ["twitch.tv", "ttvnw.net", "jtvnw.net"],
   ["twitter.com", "twimg.com", "t.co"],
   ["ua2go.com", "ual.com", "united.com", "unitedwifi.com"],
+  ["ubisoft.com", "ubi.com"],
   ["verizon.com", "verizon.net", "verizonwireless.com", "vzw.com"],
   ["vk.com", "vk.me", "vkontakte.ru"],
   ["volkskrant.nl", "persgroep.net", "persgroep.nl", "parool.nl"],
