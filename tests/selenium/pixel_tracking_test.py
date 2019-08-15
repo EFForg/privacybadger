3648bc9703bcfd7cc433960ecb2822b22c0f50f0
ablanathtanalba <daly@eff.org>
2019-08-15 11:22:30 -0700

    mod'd test, functionally works on manual testing, needs review on lingering chrome error

--- tests/selenium/pixel_tracking_test.py
+++ tests/selenium/pixel_tracking_test.py
@@ -1,20 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 import unittest
 import pbtest
 
 class pixelTrackingTesting(pbtest.PBSeleniumTest):
-	"""Tests for the cookie pixel tracking heuristic included in heuristicblocking.js"""
-	
-	TESTING_URL = ("https://gitcdn.link/repo/ablanathtanalba/pixelTrackingTestingResource/master/main.js")
+	"""Tests for the cookie pixel tracking heuristic included in heuristicblocking.js
+		- loads gitcdn resource which places a tracking cookie on page then creates an img tag
+		- img tag makes a src request carrying a substring of that tracking cookie
+		- tracking domain is caught by pixel tracking heuristic, snitch map entry is updated
+	"""
 
-	def check_to_make_sure_tracker_is_caught():
-		url = TESTING_URL
-	
-		self.load(url)
+	def test_that_tracker_is_caught(self):
+		TESTING_URL = ("https://gitcdn.link/repo/ablanathtanalba/pixelTrackingTestingResource/master/rsrc.html")
 
-		self.assertEqual(
-			self.js(
-				"return (badger.storage.snitch_map.getItem(url))"
-			)
+		CHECK_SNITCH_MAP_FOR_ENTRY = (
+				"return chrome.extension.getBackgroundPage()."
+				"badger.storage.snitch_map.getItem('github.com').includes('gitcdn.link');"
+		)
+
+		self.load_url(TESTING_URL)
+
+		# github will already appear in a pretrained badger instance for appearing on fontawesome.com
+		# instead check for it's specific appearance on gitcdn.link -- the testing resource for this
+		self.assertTrue(
+			self.js(CHECK_SNITCH_MAP_FOR_ENTRY)
 		)
 
 if __name__ == "__main__":
