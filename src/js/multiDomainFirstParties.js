aef996bfac986d28f945a60316a74ce445a4d1aa
Alexei <alexeiatyahoodotcom@gmail.com>
2019-10-09 17:11:46 -0400

    Add a couple of Spiceworks domains to MDFP.

--- src/js/multiDomainFirstParties.js
+++ src/js/multiDomainFirstParties.js
@@ -1951,6 +1951,7 @@ var multiDomainFirstPartiesArray = [
   ["soundcu.com", "netteller.com"],
   ["southerncompany.com", "southernco.com"],
   ["southparkstudios.com", "cc.com", "comedycentral.com"],
+  ["spiceworks.com", "spiceworksstatic.com"],
   [
     "spotify.com",
 
