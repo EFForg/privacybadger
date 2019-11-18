EFForg/privacybadger

commit b7e9aea694e07a1f744259308779187e4fc9bd42

Enumerate permitted content script messages.

src/js/contentscripts/supercookie.js
@@ -120,7 +120,8 @@ if (window.top == window) {
 // could then remove test workarounds like
 // https://github.com/EFForg/privacybadger/commit/39d5d0899e22d1c451d429e44553c5f9cad7fc46
 chrome.runtime.sendMessage({
-  checkEnabledAndThirdParty: window.FRAME_URL
+  type: "checkEnabledAndThirdParty",
+  frameUrl: window.FRAME_URL
 }, function (enabledAndThirdParty) {
   if (!enabledAndThirdParty) {
     return;
@@ -132,7 +133,8 @@ chrome.runtime.sendMessage({
   document.addEventListener(event_id_super_cookie, function (e) {
     // pass these on to the background page (handled by webrequest.js)
     chrome.runtime.sendMessage({
-      superCookieReport: e.detail,
+      type: "supercookieReport",
+      data: e.detail,
       frameUrl: window.FRAME_URL
     });
   });

src/js/webrequest.js
@@ -615,12 +615,40 @@ function unblockWidgetOnTab(tabId, widgetUrls) {
 
 // NOTE: sender.tab is available for content script (not popup) messages only
 function dispatcher(request, sender, sendResponse) {
-  if (request.checkEnabled) {
+
+  // messages from content scripts are to be treated with greater caution:
+  // https://groups.google.com/a/chromium.org/d/msg/chromium-extensions/0ei-UCHNm34/lDaXwQhzBAAJ
+  if (!sender.url.startsWith(chrome.runtime.getURL(""))) {
+    // reject unless it's a known content script message
+    const KNOWN_CONTENT_SCRIPT_MESSAGES = [
+      "checkDNT",
+      "checkEnabled",
+      "checkEnabledAndThirdParty",
+      "checkLocation",
+      "checkReplaceButton",
+      "checkWidgetReplacementEnabled",
+      "fpReport",
+      "getReplacementButton",
+      "supercookieReport",
+      "unblockWidget",
+    ];
+    if (!KNOWN_CONTENT_SCRIPT_MESSAGES.includes(request.type)) {
+      console.error("Rejected unknown message %o from %s", request, sender.url);
+      return sendResponse();
+    }
+  }
+
+  switch (request.type) {
+
+  case "checkEnabled": {
     sendResponse(badger.isPrivacyBadgerEnabled(
       window.extractHostFromURL(sender.tab.url)
     ));
 
-  } else if (request.checkLocation) {
+    break;
+  }
+
+  case "checkLocation": {
     if (!badger.isPrivacyBadgerEnabled(window.extractHostFromURL(sender.tab.url))) {
       return sendResponse();
     }
@@ -630,31 +658,37 @@ function dispatcher(request, sender, sendResponse) {
       return sendResponse();
     }
 
-    let requestHost = window.extractHostFromURL(request.checkLocation);
+    let requestHost = window.extractHostFromURL(request.frameUrl);
 
     // Ignore requests that aren't from a third party.
     if (!utils.isThirdPartyDomain(requestHost, window.extractHostFromURL(sender.tab.url))) {
       return sendResponse();
     }
 
-    var reqAction = checkAction(sender.tab.id, requestHost);
-    var cookieBlock = reqAction == constants.COOKIEBLOCK || reqAction == constants.USER_COOKIE_BLOCK;
-    sendResponse(cookieBlock);
+    let action = checkAction(sender.tab.id, requestHost);
+    sendResponse(action == constants.COOKIEBLOCK || action == constants.USER_COOKIE_BLOCK);
 
-  } else if (request.checkReplaceButton) {
+    break;
+  }
+
+  case "checkReplaceButton": {
     if (badger.isPrivacyBadgerEnabled(window.extractHostFromURL(sender.tab.url)) && badger.isWidgetReplacementEnabled()) {
       let widgetBlockList = getWidgetBlockList();
       sendResponse(widgetBlockList);
     }
 
-  } else if (request.unblockWidget) {
+    break;
+  }
+
+  case "unblockWidget": {
     unblockWidgetOnTab(sender.tab.id, request.buttonUrls);
     sendResponse();
+    break;
+  }
 
-  } else if (request.getReplacementButton) {
-
+  case "getReplacementButton": {
     let button_path = chrome.runtime.getURL(
-      "skin/socialwidgets/" + request.getReplacementButton);
+      "skin/socialwidgets/" + request.imagePath);
 
     let image_type = button_path.slice(button_path.lastIndexOf('.') + 1);
 
@@ -679,36 +713,48 @@ function dispatcher(request, sender, sendResponse) {
 
     // indicate this is an async response to chrome.runtime.onMessage
     return true;
+  }
 
-  // Canvas fingerprinting
-  } else if (request.fpReport) {
-    if (!badger.isPrivacyBadgerEnabled(window.extractHostFromURL(sender.tab.url))) { return; }
-    if (Array.isArray(request.fpReport)) {
-      request.fpReport.forEach(function (msg) {
+  case "fpReport": {
+    if (!badger.isPrivacyBadgerEnabled(window.extractHostFromURL(sender.tab.url))) {
+      return sendResponse();
+    }
+    if (Array.isArray(request.data)) {
+      request.data.forEach(function (msg) {
         recordFingerprinting(sender.tab.id, msg);
       });
     } else {
-      recordFingerprinting(sender.tab.id, request.fpReport);
+      recordFingerprinting(sender.tab.id, request.data);
     }
 
-  } else if (request.superCookieReport) {
-    if (badger.hasSuperCookie(request.superCookieReport)) {
+    break;
+  }
+
+  case "supercookieReport": {
+    if (badger.hasSuperCookie(request.data)) {
       recordSuperCookie(sender.tab.id, request.frameUrl);
     }
+    break;
+  }
 
-  } else if (request.checkEnabledAndThirdParty) {
+  case "checkEnabledAndThirdParty": {
     let tab_host = window.extractHostFromURL(sender.tab.url),
-      frame_host = window.extractHostFromURL(request.checkEnabledAndThirdParty);
+      frame_host = window.extractHostFromURL(request.frameUrl);
 
     sendResponse(badger.isPrivacyBadgerEnabled(tab_host) && utils.isThirdPartyDomain(frame_host, tab_host));
 
-  } else if (request.checkWidgetReplacementEnabled) {
+    break;
+  }
+
+  case "checkWidgetReplacementEnabled": {
     sendResponse(
       badger.isPrivacyBadgerEnabled(window.extractHostFromURL(sender.tab.url)) &&
       badger.isWidgetReplacementEnabled()
     );
+    break;
+  }
 
-  } else if (request.type == "getPopupData") {
+  case "getPopupData": {
     let tab_id = request.tabId,
       tab_url = request.tabUrl,
       tab_host = window.extractHostFromURL(tab_url),
@@ -728,7 +774,10 @@ function dispatcher(request, sender, sendResponse) {
       trackerCount: has_tab_data && badger.getTrackerCount(tab_id)
     });
 
-  } else if (request.type == "getOptionsData") {
+    break;
+  }
+
+  case "getOptionsData": {
     sendResponse({
       disabledSites: badger.getDisabledSites(),
       isCheckingDNTPolicyEnabled: badger.isCheckingDNTPolicyEnabled(),
@@ -742,35 +791,50 @@ function dispatcher(request, sender, sendResponse) {
       webRTCAvailable: badger.webRTCAvailable,
     });
 
-  } else if (request.type == "resetData") {
+    break;
+  }
+
+  case "resetData": {
     badger.storage.clearTrackerData();
     badger.loadSeedData();
     sendResponse();
+    break;
+  }
 
-  } else if (request.type == "removeAllData") {
+  case "removeAllData": {
     badger.storage.clearTrackerData();
     sendResponse();
+    break;
+  }
 
-  } else if (request.type == "seenComic") {
+  case "seenComic": {
     badger.getSettings().setItem("seenComic", true);
+    break;
+  }
 
-  } else if (request.type == "activateOnSite") {
+  case "activateOnSite": {
     badger.enablePrivacyBadgerForOrigin(request.tabHost);
     badger.refreshIconAndContextMenu(request.tabId, request.tabUrl);
     sendResponse();
+    break;
+  }
 
-  } else if (request.type == "deactivateOnSite") {
+  case "deactivateOnSite": {
     badger.disablePrivacyBadgerForOrigin(request.tabHost);
     badger.refreshIconAndContextMenu(request.tabId, request.tabUrl);
     sendResponse();
+    break;
+  }
 
-  } else if (request.type == "revertDomainControl") {
+  case "revertDomainControl": {
     badger.storage.revertUserAction(request.origin);
     sendResponse({
       origins: badger.storage.getTrackingDomains()
     });
+    break;
+  }
 
-  } else if (request.type == "downloadCloud") {
+  case "downloadCloud": {
     chrome.storage.sync.get("disabledSites", function (store) {
       if (chrome.runtime.lastError) {
         sendResponse({success: false, message: chrome.runtime.lastError.message});
@@ -791,10 +855,12 @@ function dispatcher(request, sender, sendResponse) {
         });
       }
     });
-    //indicate this is an async response to chrome.runtime.onMessage
-    return true;
 
-  } else if (request.type == "uploadCloud") {
+    // indicate this is an async response to chrome.runtime.onMessage
+    return true;
+  }
+
+  case "uploadCloud": {
     let obj = {};
     obj.disabledSites = badger.getDisabledSites();
     chrome.storage.sync.set(obj, function () {
@@ -804,10 +870,11 @@ function dispatcher(request, sender, sendResponse) {
         sendResponse({success: true});
       }
     });
-    //indicate this is an async response to chrome.runtime.onMessage
+    // indicate this is an async response to chrome.runtime.onMessage
     return true;
+  }
 
-  } else if (request.type == "savePopupToggle") {
+  case "savePopupToggle": {
     let domain = request.origin,
       action = request.action;
 
@@ -816,22 +883,29 @@ function dispatcher(request, sender, sendResponse) {
     // update cached tab data so that a reopened popup displays correct state
     badger.tabData[request.tabId].origins[domain] = "user_" + action;
 
-  } else if (request.type == "saveOptionsToggle") {
+    break;
+  }
+
+  case "saveOptionsToggle": {
     // called when the user manually sets a slider on the options page
     badger.saveAction(request.action, request.origin);
     sendResponse({
       origins: badger.storage.getTrackingDomains()
     });
+    break;
+  }
 
-  } else if (request.type == "mergeUserData") {
+  case "mergeUserData": {
     // called when a user uploads data exported from another Badger instance
     badger.mergeUserData(request.data);
     sendResponse({
       disabledSites: badger.getDisabledSites(),
       origins: badger.storage.getTrackingDomains(),
     });
+    break;
+  }
 
-  } else if (request.type == "updateSettings") {
+  case "updateSettings": {
     const settings = badger.getSettings();
     for (let key in request.data) {
       if (badger.defaultSettings.hasOwnProperty(key)) {
@@ -841,42 +915,56 @@ function dispatcher(request, sender, sendResponse) {
       }
     }
     sendResponse();
+    break;
+  }
 
-  } else if (request.type == "updateBadge") {
+  case "updateBadge": {
     let tab_id = request.tab_id;
     badger.updateBadge(tab_id);
     sendResponse();
+    break;
+  }
 
-  } else if (request.type == "disablePrivacyBadgerForOrigin") {
+  case "disablePrivacyBadgerForOrigin": {
     badger.disablePrivacyBadgerForOrigin(request.domain);
     sendResponse({
       disabledSites: badger.getDisabledSites()
     });
+    break;
+  }
 
-  } else if (request.type == "enablePrivacyBadgerForOriginList") {
+  case "enablePrivacyBadgerForOriginList": {
     request.domains.forEach(function (domain) {
       badger.enablePrivacyBadgerForOrigin(domain);
     });
     sendResponse({
       disabledSites: badger.getDisabledSites()
     });
+    break;
+  }
 
-  } else if (request.type == "removeOrigin") {
+  case "removeOrigin": {
     badger.storage.getBadgerStorageObject("snitch_map").deleteItem(request.origin);
     badger.storage.getBadgerStorageObject("action_map").deleteItem(request.origin);
     sendResponse({
       origins: badger.storage.getTrackingDomains()
     });
+    break;
+  }
 
-  } else if (request.type == "saveErrorText") {
+  case "saveErrorText": {
     let activeTab = badger.tabData[request.tabId];
     activeTab.errorText = request.errorText;
+    break;
+  }
 
-  } else if (request.type == "removeErrorText") {
+  case "removeErrorText": {
     let activeTab = badger.tabData[request.tabId];
     delete activeTab.errorText;
+    break;
+  }
 
-  } else if (request.checkDNT) {
+  case "checkDNT": {
     // called from contentscripts/dnt.js to check if we should enable it
     sendResponse(
       badger.isDNTSignalEnabled()
@@ -884,6 +972,9 @@ function dispatcher(request, sender, sendResponse) {
         window.extractHostFromURL(sender.tab.url)
       )
     );
+    break;
+  }
+
   }
 }
 

.eslintrc.yml
@@ -27,7 +27,6 @@ rules:
     - error
     - 2
     - outerIIFEBody: 0
-      SwitchCase: 1
   keyword-spacing: error
   linebreak-style:
     - error

src/js/contentscripts/clobbercookie.js
@@ -32,7 +32,10 @@ if (window.top == window) {
 }
 
 // TODO race condition; fix waiting on https://crbug.com/478183
-chrome.runtime.sendMessage({ checkLocation: window.FRAME_URL }, function (blocked) {
+chrome.runtime.sendMessage({
+  type: "checkLocation",
+  frameUrl: window.FRAME_URL
+}, function (blocked) {
   if (blocked) {
     var code = '('+ function() {
       document.__defineSetter__("cookie", function(/*value*/) { });

src/js/contentscripts/clobberlocalstorage.js
@@ -32,7 +32,10 @@ if (window.top == window) {
 }
 
 // TODO race condition; fix waiting on https://crbug.com/478183
-chrome.runtime.sendMessage({ checkLocation: window.FRAME_URL }, function (blocked) {
+chrome.runtime.sendMessage({
+  type: "checkLocation",
+  frameUrl: window.FRAME_URL
+}, function (blocked) {
   if (blocked) {
     // https://stackoverflow.com/questions/49092423/how-to-break-on-localstorage-changes
     var code =

src/js/contentscripts/dnt.js
@@ -50,7 +50,7 @@ if (document instanceof HTMLDocument === false && (
 
 // TODO race condition; fix waiting on https://crbug.com/478183
 chrome.runtime.sendMessage({
-  checkDNT: true
+  type: "checkDNT"
 }, function (enabled) {
   if (enabled) {
     window.injectScript(getPageScript());

src/js/contentscripts/fingerprinting.js
@@ -313,28 +313,29 @@ if (document instanceof HTMLDocument === false && (
 }
 
 // TODO race condition; fix waiting on https://crbug.com/478183
-chrome.runtime.sendMessage({checkEnabled: true},
-  function (enabled) {
-    if (!enabled) {
-      return;
-    }
-    /**
-     * Communicating to webrequest.js
-     */
-    var event_id = Math.random();
-
-    // listen for messages from the script we are about to insert
-    document.addEventListener(event_id, function (e) {
-      // pass these on to the background page
-      chrome.runtime.sendMessage({
-        fpReport: e.detail
-      });
-    });
-
-    window.injectScript(getFpPageScript(), {
-      event_id: event_id
-    });
+chrome.runtime.sendMessage({
+  type: "checkEnabled"
+}, function (enabled) {
+  if (!enabled) {
+    return;
   }
-);
+  /**
+   * Communicating to webrequest.js
+   */
+  var event_id = Math.random();
+
+  // listen for messages from the script we are about to insert
+  document.addEventListener(event_id, function (e) {
+    // pass these on to the background page
+    chrome.runtime.sendMessage({
+      type: "fpReport",
+      data: e.detail
+    });
+  });
+
+  window.injectScript(getFpPageScript(), {
+    event_id: event_id
+  });
+});
 
 }());

src/js/contentscripts/socialwidgets.js
@@ -102,7 +102,8 @@ function createReplacementButtonImage(tracker, trackerElem, callback) {
   // don't have image data cached yet, get it from the background page
   buttonData.loading = true;
   chrome.runtime.sendMessage({
-    getReplacementButton: buttonData.imagePath
+    type: "getReplacementButton",
+    imagePath: buttonData.imagePath
   }, function (response) {
     buttonData.buttonUrl = response; // cache image data
     _createReplacementButtonImageCallback(tracker, trackerElem, callback);
@@ -416,7 +417,9 @@ function replaceIndividualButton(tracker) {
  *                            replaced
  */
 function getTrackerData(callback) {
-  chrome.runtime.sendMessage({checkReplaceButton: true}, function(response) {
+  chrome.runtime.sendMessage({
+    type: "checkReplaceButton"
+  }, function (response) {
     if (response) {
       for (const key in response.translations) {
         TRANSLATIONS[key] = response.translations[key];
@@ -435,7 +438,7 @@ function getTrackerData(callback) {
  */
 function unblockTracker(buttonUrls, callback) {
   let request = {
-    unblockWidget: true,
+    type: "unblockWidget",
     buttonUrls: buttonUrls
   };
   chrome.runtime.sendMessage(request, callback);
@@ -455,9 +458,9 @@ if (document instanceof HTMLDocument === false && (
 }
 
 chrome.runtime.sendMessage({
-  checkWidgetReplacementEnabled: true
-}, function (checkWidgetReplacementEnabled) {
-  if (!checkWidgetReplacementEnabled) {
+  type: "checkWidgetReplacementEnabled"
+}, function (enabled) {
+  if (!enabled) {
     return;
   }
   initialize();

commit 1518d01a66deb04b5dc27323fc8d73e78bc8b8a5

Add TODO to look into localStorage detection bug
This hides "window.injectScript undefined" console errors in Chrome that
started in b7e9aea694e07a1f744259308779187e4fc9bd42

The aforementioned commit merely made the problem more visible, however.
The underlying issue (FRAME_URL/injectScript from the utils.js content
script sometimes being undefined in the supercookie.js content script)
was already present at that point.

src/js/contentscripts/supercookie.js
@@ -119,6 +119,9 @@ if (window.top == window) {
 //
 // could then remove test workarounds like
 // https://github.com/EFForg/privacybadger/commit/39d5d0899e22d1c451d429e44553c5f9cad7fc46
+
+// TODO sometimes contentscripts/utils.js isn't here?!
+// TODO window.FRAME_URL / window.injectScript are undefined ...
 chrome.runtime.sendMessage({
   type: "checkEnabledAndThirdParty",
   frameUrl: window.FRAME_URL

src/js/webrequest.js
@@ -682,14 +682,15 @@ function dispatcher(request, sender, sendResponse) {
       return sendResponse();
     }
 
-    let requestHost = window.extractHostFromURL(request.frameUrl);
+    let frame_host = window.extractHostFromURL(request.frameUrl),
+      tab_host = window.extractHostFromURL(sender.tab.url);
 
     // Ignore requests that aren't from a third party.
-    if (!utils.isThirdPartyDomain(requestHost, window.extractHostFromURL(sender.tab.url))) {
+    if (!frame_host || !utils.isThirdPartyDomain(frame_host, tab_host)) {
       return sendResponse();
     }
 
-    let action = checkAction(sender.tab.id, requestHost);
+    let action = checkAction(sender.tab.id, frame_host);
     sendResponse(action == constants.COOKIEBLOCK || action == constants.USER_COOKIE_BLOCK);
 
     break;
@@ -770,7 +771,7 @@ function dispatcher(request, sender, sendResponse) {
   }
 
   case "supercookieReport": {
-    if (badger.hasSuperCookie(request.data)) {
+    if (request.frameUrl && badger.hasSuperCookie(request.data)) {
       recordSuperCookie(sender.tab.id, request.frameUrl);
     }
     break;
@@ -780,7 +781,9 @@ function dispatcher(request, sender, sendResponse) {
     let tab_host = window.extractHostFromURL(sender.tab.url),
       frame_host = window.extractHostFromURL(request.frameUrl);
 
-    sendResponse(badger.isPrivacyBadgerEnabled(tab_host) && utils.isThirdPartyDomain(frame_host, tab_host));
+    sendResponse(frame_host &&
+      badger.isPrivacyBadgerEnabled(tab_host) &&
+      utils.isThirdPartyDomain(frame_host, tab_host));
 
     break;
   }