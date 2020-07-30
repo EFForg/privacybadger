0a8a9a7a7bfc34bd5d2385d1df9f60f3987ef6cb
Alexei <alexeiatyahoodotcom@gmail.com>
2020-07-30 16:46:27 -0400

    Update third-party frame URL in fixture

--- tests/selenium/website_testbed/first-party.js
+++ tests/selenium/website_testbed/first-party.js
@@ -1,26 +1,25 @@
-
 function setExpire() {
-  var now = new Date();
-  var time = now.getTime();
-  var expireTime = time + 864000;
-  now.setTime(expireTime);
-  return ";expires=" + now.toGMTString();
+	var now = new Date();
+	var time = now.getTime();
+	var expireTime = time + 864000;
+	now.setTime(expireTime);
+	return ";expires=" + now.toGMTString();
 }
 
 function setPath() {
-  return ";path=/";
+	return ";path=/";
 }
 
 function setSameSite() {
-  return ";SameSite=None;Secure";
+	return ";SameSite=None;Secure";
 }
 
 function updateCookie() {
-  var oldcookie = document.cookie;
-  var val = "1234567890";
-  console.log("read cookie: " + oldcookie);
-  document.cookie = "localtest=" + encodeURIComponent(val) + setExpire() + setPath() + setSameSite();
-  console.log("updating cookie to:" + document.cookie);
+	var oldcookie = document.cookie;
+	var val = "1234567890";
+	console.log("read cookie: " + oldcookie);
+	document.cookie = "localtest=" + encodeURIComponent(val) + setExpire() + setPath() + setSameSite();
+	console.log("updating cookie to:" + document.cookie);
 }
 
 updateCookie();
