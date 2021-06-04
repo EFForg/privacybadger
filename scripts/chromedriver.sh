d3a23cf7b02465ee3d0fcdab434bdc95b54872b5
Alexei <alexeiatyahoodotcom@gmail.com>
2021-06-04 16:36:59 -0400

    Detect failure to discover ChromeDriver version

--- scripts/chromedriver.sh
+++ scripts/chromedriver.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+# installs the appropriate version of ChromeDriver for a given Chrome version
+
 # stop on errors (nonzero exit codes), uninitialized vars
 set -eu
 
 TEMPFILE=$(mktemp)
-CHROME="$1"
+CHROME="$1" # google-chrome or google-chrome-beta or ...
 
 trap 'rm $TEMPFILE' EXIT
 
-# install the appropriate version of ChromeDriver
 chrome_version=$("$CHROME" --product-version | cut -d . -f 1-3)
+
 chromedriver_version_url=https://chromedriver.storage.googleapis.com/LATEST_RELEASE_"$chrome_version"
+set +e
 chromedriver_version=$(wget "$chromedriver_version_url" -q -O -)
+set -e
+if [ -z "$chromedriver_version" ]; then
+  echo "Failed to retrieve ChromeDriver version!"
+  exit 1
+fi
+
 echo "Setting up ChromeDriver version $chromedriver_version ..."
 chromedriver_url=https://chromedriver.storage.googleapis.com/"$chromedriver_version"/chromedriver_linux64.zip
 wget -q -O "$TEMPFILE" "$chromedriver_url"
