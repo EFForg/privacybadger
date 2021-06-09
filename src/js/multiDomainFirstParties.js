0d572629c06f4d6a75f3da1dd2815ae1fb5a92ea
Alexei <alexeiatyahoodotcom@gmail.com>
2021-06-09 12:50:45 -0400

    Add TikTok domains to MDFP list

--- src/js/multiDomainFirstParties.js
+++ src/js/multiDomainFirstParties.js
@@ -3701,6 +3701,7 @@ let multiDomainFirstPartiesArray = [
   ["telegram.org", "telegram.me", "t.me"],
   ["telekom.com", "t-online.de"],
   ["tesla.com", "teslamotors.com"],
+  ["tiktok.com", "byteoversea.com", "ibytedtos.com", "tiktokcdn.com", "tiktokv.com"],
   [
     "toyota.com",
 
