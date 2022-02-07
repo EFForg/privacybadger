0d54652bfedb3e1fc37bd0d6ae48f962eb3bea47
danielrussellla <contactdanielrussell@gmail.com>
2022-02-07 12:38:32 -0600

    add copyright comment to top of relevant js files for sp-privacy

--- sp-privacy/lib/cjs/index.js
+++ sp-privacy/lib/cjs/index.js
@@ -1,4 +1,22 @@
 "use strict";
+
+/*
+ * Copyright (C) 2022 Surfboard Holding B.V. <https://www.startpage.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
