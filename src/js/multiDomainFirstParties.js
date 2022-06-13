4364717d29bf5569a8d87f37b2bd070ac37eedef
Alexei <alexeiatyahoodotcom@gmail.com>
2022-06-13 17:54:34 -0400

    Add a couple of Bandcamp domains to MDFP list

--- src/js/multiDomainFirstParties.js
+++ src/js/multiDomainFirstParties.js
@@ -1211,7 +1211,7 @@ let multiDomainFirstPartiesArray = [
 
     "envato-static.com",
   ],
-  ["epicgames.com", "unrealengine.com"],
+  ["epicgames.com", "unrealengine.com", "bandcamp.com", "bcbits.com"],
   [
     "eventbrite.com",
 
