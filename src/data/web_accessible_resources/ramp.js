e90bbf6506b3eac672e0849034981284679318be
errorlifeisnull <120217293+errorlifeisnull@users.noreply.github.com>
2022-12-09 12:03:22 -0800

    Fixing bestiefy.com

---
+++ src/data/web_accessible_resources/ramp.js
@@ -0,0 +1,23 @@
+window.ramp = {
+  addUnits: function () {
+    return;
+  },
+  destroyUnits: function () {
+    return;
+  },
+  displayUnits: function () {
+    return;
+  },
+};
+
+window.tyche = {
+  addUnits: function () {
+    return;
+  },
+  destroyUnits: function () {
+    return;
+  },
+  displayUnits: function () {
+    return;
+  },
+};
