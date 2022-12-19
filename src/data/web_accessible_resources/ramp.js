2fed2b1c471f9107f227a6499d0082f5332196be
errorlifeisnull <120217293+errorlifeisnull@users.noreply.github.com>
2022-12-19 13:35:34 -0800

    update ramp to fix better
    https://github.com/EFForg/privacybadger/pull/2865#issuecomment-1355151256

--- src/data/web_accessible_resources/ramp.js
+++ src/data/web_accessible_resources/ramp.js
@@ -1,23 +1,17 @@
-window.ramp = {
-  addUnits: function () {
-    return;
-  },
-  destroyUnits: function () {
-    return;
-  },
-  displayUnits: function () {
-    return;
-  },
-};
-
-window.tyche = {
-  addUnits: function () {
-    return;
-  },
-  destroyUnits: function () {
-    return;
-  },
-  displayUnits: function () {
-    return;
-  },
-};
+// https://developers.playwire.com/api/docs/ads/getting-started-ads.html
+// https://github.com/EFForg/privacybadger/pull/2865#issuecomment-1355151256
+(function () {
+  function noopfn() {}
+  function checkForOnReady() {
+    if (!window.ramp) {
+      window.ramp = {};
+    }
+    window.ramp.addUnits = () => new Promise(noopfn);
+    window.ramp.displayUnits = noopfn;
+    window.ramp.destroyUnits = noopfn;
+    if (typeof window.ramp.onReady == "function") {
+      window.ramp.onReady();
+    }
+  }
+  setTimeout(checkForOnReady, 10);
+})();
