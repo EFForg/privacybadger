463894198affe6d136a1c14c1e16429442efeaa8
Alexei <alexeiatyahoodotcom@gmail.com>
2023-04-07 14:53:25 -0400

    Use latest ChromeDriver version as fallback

--- scripts/chromedriver.sh
+++ scripts/chromedriver.sh
@@ -2,9 +2,6 @@
 
 # installs the appropriate version of ChromeDriver for a given Chrome version
 
-# stop on errors (nonzero exit codes), uninitialized vars
-set -eu
-
 TEMPFILE=$(mktemp)
 CHROME="$1" # google-chrome or google-chrome-beta or ...
 
@@ -13,12 +10,16 @@ trap 'rm $TEMPFILE' EXIT
 chrome_version=$("$CHROME" --product-version | cut -d . -f 1-3)
 
 chromedriver_version_url=https://chromedriver.storage.googleapis.com/LATEST_RELEASE_"$chrome_version"
-set +e
 chromedriver_version=$(wget "$chromedriver_version_url" -q -O -)
-set -e
 if [ -z "$chromedriver_version" ]; then
-  echo "Failed to retrieve ChromeDriver version!"
-  exit 1
+  echo "Failed to retrieve ChromeDriver version for $chrome_version"
+
+  chromedriver_version_url=https://chromedriver.storage.googleapis.com/LATEST_RELEASE
+  chromedriver_version=$(wget "$chromedriver_version_url" -q -O -)
+  if [ -z "$chromedriver_version" ]; then
+    echo "Failed to retrieve ChromeDriver version!"
+    exit 1
+  fi
 fi
 
 echo "Setting up ChromeDriver version $chromedriver_version ..."
