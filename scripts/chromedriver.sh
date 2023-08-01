b8d20e28027ee445e2e1cfd71ed597eaf1254ada
Alexei <alexeiatyahoodotcom@gmail.com>
2023-08-01 17:24:26 -0400

    Fix ChromeDriver install script for Chrome 115+
    https://chromedriver.chromium.org/downloads/version-selection
    https://developer.chrome.com/blog/chrome-for-testing/

--- scripts/chromedriver.sh
+++ scripts/chromedriver.sh
@@ -2,34 +2,55 @@
 
 # installs the appropriate version of ChromeDriver for a given Chrome version
 
-TEMPFILE=$(mktemp)
-CHROME="${1:-google-chrome}" # google-chrome or google-chrome-beta or ...
+TEMPDIR=$(mktemp -d)
+trap 'rm -rf $TEMPDIR' EXIT
 
-trap 'rm $TEMPFILE' EXIT
+# one of google-chrome, google-chrome-stable,
+# google-chrome-beta, or google-chrome-unstable
+CHROME="${1:-google-chrome}"
 
-chrome_version=$("$CHROME" --product-version | cut -d . -f 1-3)
+major_version=$("$CHROME" --product-version | cut -d . -f 1)
 
-chromedriver_version_url=https://chromedriver.storage.googleapis.com/LATEST_RELEASE_"$chrome_version"
-chromedriver_version=$(wget "$chromedriver_version_url" -q -O -)
+if [ -z "$major_version" ]; then
+  echo "Failed to look up version of $CHROME"
+  exit 1
+fi
+
+if [ "$major_version" -lt 115 ]; then
+  echo "This script supports installing ChromeDriver for Chrome 115+ only"
+  exit 1
+fi
+
+channel=Stable
+case "$CHROME" in
+  *beta*)
+    channel=Beta
+    ;;
+  *unstable*)
+    channel=Dev
+    ;;
+esac
+json_url=https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json
+json=$(wget "$json_url" -q -O -)
+chromedriver_version=$(echo "$json" | python3 -c "import sys, json; print(json.load(sys.stdin)['channels']['$channel']['version'])")
+chromedriver_url=$(echo "$json" | python3 -c "import sys, json; print(next(i['url'] for i in json.load(sys.stdin)['channels']['$channel']['downloads']['chromedriver'] if i['platform'] == 'linux64'))")
+
 if [ -z "$chromedriver_version" ]; then
-  echo "Failed to retrieve ChromeDriver version for $chrome_version"
-
-  chromedriver_version_url=https://chromedriver.storage.googleapis.com/LATEST_RELEASE
-  chromedriver_version=$(wget "$chromedriver_version_url" -q -O -)
-  if [ -z "$chromedriver_version" ]; then
-    echo "Failed to retrieve ChromeDriver version!"
-    exit 1
-  fi
+  echo "Failed to retrieve ChromeDriver version"
+  exit 1
 fi
 
 echo "Setting up ChromeDriver version $chromedriver_version ..."
-chromedriver_url=https://chromedriver.storage.googleapis.com/"$chromedriver_version"/chromedriver_linux64.zip
-wget -q -O "$TEMPFILE" "$chromedriver_url"
-sudo unzip -q -o "$TEMPFILE" chromedriver -d /usr/local/bin/
-sudo chmod a+x /usr/local/bin/chromedriver
+
+wget -q -O "$TEMPDIR/chromedriver.zip" "$chromedriver_url" \
+  && unzip -q -o "$TEMPDIR/chromedriver.zip" chromedriver-linux64/chromedriver -d "$TEMPDIR" \
+  && sudo mv "$TEMPDIR/chromedriver-linux64/chromedriver" /usr/local/bin/chromedriver \
+  && sudo chmod a+x /usr/local/bin/chromedriver
 
 # check that chromedriver is now present
 type chromedriver >/dev/null 2>&1 || {
   echo "Failed to install ChromeDriver!"
   exit 1
 }
+
+chromedriver --version
