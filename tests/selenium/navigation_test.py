9d8e9bb5f94fdbe12633acb02a4136e4b17530f1
Alexei <alexeiatyahoodotcom@gmail.com>
2024-03-27 16:36:45 -0400

    Fix wait condition in beacon attribution test

--- tests/selenium/navigation_test.py
+++ tests/selenium/navigation_test.py
@@ -47,7 +47,6 @@ class NavigationTest(pbtest.PBSeleniumTest):
         self.driver.refresh()
         self.driver.switch_to.window(self.driver.window_handles[-1]) 
         domains = pbtest.retry_until(partial(self.get_trackers, FIXTURE_URL),
-                                     tester=lambda x: x == "allow",
                                      times=3)
         assert domains == {THIRD_PARTY_HOST: "allow"}, "beacon should have fired"
 
