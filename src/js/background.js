f4ac5abcecda936595be5a8d88918af9dd2db0b8
Alexei <alexeiatyahoodotcom@gmail.com>
2024-05-28 17:17:09 -0400

    Chrome MV3 WIP

--- src/js/background.js
+++ src/js/background.js
@@ -19,7 +19,6 @@ import { extractHostFromURL, getBaseDomain } from "../lib/basedomain.js";
 
 import { log } from "./bootstrap.js";
 import constants from "./constants.js";
-import FirefoxAndroid from "./firefoxandroid.js";
 import HeuristicBlocking from "./heuristicblocking.js";
 import incognito from "./incognito.js";
 import widgetLoader from "./socialwidgetloader.js";
@@ -95,7 +94,6 @@ function Badger(from_qunit) {
     incognito.startListeners();
     webrequest.startListeners();
     HeuristicBlocking.startListeners();
-    FirefoxAndroid.startListeners();
     startBackgroundListeners();
   }
 
@@ -122,12 +120,8 @@ function Badger(from_qunit) {
     await pbconfigPromise;
     let seedDataPromise = self.updateTrackerData().catch(console.error);
 
-    // set badge text color to white in Firefox 63+
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1474110
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1424620
-    if (utils.hasOwn(chrome.browserAction, 'setBadgeTextColor')) {
-      chrome.browserAction.setBadgeTextColor({ color: "#fff" });
-    }
+    // set badge text color to white
+    chrome.action.setBadgeTextColor({ color: "#fff" });
 
     // wait for async functions (seed data, yellowlist, ...) to resolve
     await widgetListPromise;
@@ -136,15 +130,15 @@ function Badger(from_qunit) {
     if (self.isFirstRun || self.isUpdate || !self.getPrivateSettings().getItem('doneLoadingSeed')) {
       // block all widget domains
       // only need to do this when the widget list could have gotten updated
-      window.DATA_LOAD_IN_PROGRESS = true;
+      globalThis.DATA_LOAD_IN_PROGRESS = true;
       self.blockWidgetDomains();
       self.blockPanopticlickDomains();
-      window.DATA_LOAD_IN_PROGRESS = false;
+      globalThis.DATA_LOAD_IN_PROGRESS = false;
     }
 
     log("Initialization complete");
     self.INITIALIZED = true;
-    window.DEBUG = false;
+    globalThis.DEBUG = false;
 
     if (self.criticalError == "Privacy Badger failed to initialize") {
       delete self.criticalError;
@@ -949,10 +943,6 @@ Badger.prototype = {
    * @param {Number} [tab_id] the tab to update the badge for; defaults to active tab
    */
   updateBadge: function (tab_id = null) {
-    if (!FirefoxAndroid.hasBadgeSupport) {
-      return;
-    }
-
     let self = this;
 
     function _update(tab) {
@@ -969,8 +959,8 @@ Badger.prototype = {
       }
 
       if (self.criticalError) {
-        chrome.browserAction.setBadgeBackgroundColor({tabId: tab_id, color: "#cc0000"});
-        chrome.browserAction.setBadgeText({tabId: tab_id, text: "!"});
+        chrome.action.setBadgeBackgroundColor({ tabId: tab_id, color: "#cc0000" });
+        chrome.action.setBadgeText({ tabId: tab_id, text: "!" });
         return;
       }
 
@@ -982,19 +972,19 @@ Badger.prototype = {
         !self.getSettings().getItem("showCounter") ||
         !self.isPrivacyBadgerEnabled(self.tabData.getFrameData(tab_id).host)
       ) {
-        chrome.browserAction.setBadgeText({tabId: tab_id, text: ""});
+        chrome.action.setBadgeText({ tabId: tab_id, text: "" });
         return;
       }
 
       let count = self.getTrackerCount(tab_id);
 
       if (count === 0) {
-        chrome.browserAction.setBadgeText({tabId: tab_id, text: ""});
+        chrome.action.setBadgeText({ tabId: tab_id, text: "" });
         return;
       }
 
-      chrome.browserAction.setBadgeBackgroundColor({tabId: tab_id, color: "#ec9329"});
-      chrome.browserAction.setBadgeText({tabId: tab_id, text: count + ""});
+      chrome.action.setBadgeBackgroundColor({ tabId: tab_id, color: "#ec9329" });
+      chrome.action.setBadgeText({ tabId: tab_id, text: count + "" });
     }
 
     if (tab_id === null) {
@@ -1213,7 +1203,7 @@ Badger.prototype = {
    * @param {String} tab_url The tab URL to set the badger icon for
    */
   updateIcon: function (tab_id, tab_url) {
-    if (!tab_id || !tab_url || !FirefoxAndroid.hasPopupSupport) {
+    if (!tab_id || !tab_url) {
       return;
     }
 
@@ -1235,7 +1225,7 @@ Badger.prototype = {
       };
     }
 
-    chrome.browserAction.setIcon({tabId: tab_id, path: iconFilename});
+    chrome.action.setIcon({ tabId: tab_id, path: iconFilename });
   }
 
 };
@@ -1266,4 +1256,4 @@ function startBackgroundListeners() {
   });
 }
 
-let badger = window.badger = new Badger(document.location.pathname == "/tests/index.html");
+let badger = globalThis.badger = new Badger(globalThis.document && globalThis.document.location.pathname == "/tests/index.html");
