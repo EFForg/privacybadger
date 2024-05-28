fbee8d4b42ec6369124e17e9e50cc6dff26b2025
Alexei <alexeiatyahoodotcom@gmail.com>
2024-05-28 17:17:34 -0400

    Remove static block and surrogate rulesets
    All tracker blocking and request modification is now done
    entirely with dynamic rules.
    
    This removes significant complexity.

--- src/js/webrequest.js
+++ src/js/webrequest.js
@@ -1432,7 +1432,7 @@ function dispatcher(request, sender, sendResponse) {
   }
 
   case "resetData": {
-    badger.storage.clearTrackerData(function () {
+    badger.storage.clearTrackerData().then(function () {
       badger.loadSeedData().then(function () {
         globalThis.DATA_LOAD_IN_PROGRESS = true;
         badger.blockWidgetDomains();
@@ -1448,9 +1448,8 @@ function dispatcher(request, sender, sendResponse) {
   }
 
   case "removeAllData": {
-    badger.storage.clearTrackerData(sendResponse);
-    // indicate this is an async response to chrome.runtime.onMessage
-    return true;
+    badger.storage.clearTrackerData().then(sendResponse);
+    return true; // async chrome.runtime.onMessage response
   }
 
   // used by tests
