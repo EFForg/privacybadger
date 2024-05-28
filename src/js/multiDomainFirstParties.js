b04366d84d358032aadbce30b8807c4f8411e531
Alexei <alexeiatyahoodotcom@gmail.com>
2024-05-28 17:17:19 -0400

    DNR WIP

--- src/js/multiDomainFirstParties.js
+++ src/js/multiDomainFirstParties.js
@@ -6300,7 +6300,15 @@ let _domainLookup = makeDomainLookup(multiDomainFirstPartiesArray);
  */
 let isMultiDomainFirstParty = makeIsMultiDomainFirstParty(_domainLookup);
 
+function getEntityList(domain) {
+  if (domain in _domainLookup) {
+    return Array.from(_domainLookup[domain]);
+  }
+  return [];
+}
+
 export default {
+  getEntityList,
   isMultiDomainFirstParty,
   makeDomainLookup,
   makeIsMultiDomainFirstParty,
