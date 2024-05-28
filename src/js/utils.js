d9a399a71c6cd9a325667aaa222c8f8496d738ff
Alexei <alexeiatyahoodotcom@gmail.com>
2024-05-28 17:17:09 -0400

    Chrome MV3 WIP

--- src/js/utils.js
+++ src/js/utils.js
@@ -319,7 +319,7 @@ function rateLimit(fn, interval, context) {
     if (queue.length) {
       item = queue.splice(0, 1)[0];
       fn.apply(item.context, item.arguments); // invoke fn
-      timer_id = window.setTimeout(timeEnd, interval);
+      timer_id = setTimeout(timeEnd, interval);
     } else {
       canInvoke = true;
     }
@@ -327,7 +327,7 @@ function rateLimit(fn, interval, context) {
 
   // useful for debugging
   limited.cancel = function () {
-    window.clearTimeout(timer_id);
+    clearTimeout(timer_id);
     queue = [];
     canInvoke = true;
   };
@@ -340,12 +340,92 @@ function buf2hex(buffer) { // buffer is an ArrayBuffer
 }
 
 function sha1(input, callback) {
-  return window.crypto.subtle.digest(
+  return globalThis.crypto.subtle.digest(
     { name: "SHA-1", },
     new TextEncoder().encode(input)
   ).then(hashed => {
     return callback(buf2hex(hashed));
   });
+}
+
+/**
+ * From https://github.com/mathiasbynens/CSS.escape/blob/4b25c283eaf4dd443f44a7096463e973d56dd1b2/css.escape.js
+ * https://mths.be/cssescape v1.5.1 by @mathias | MIT license
+ */
+function cssEscape(value) {
+  if (arguments.length == 0) {
+    throw new TypeError('`CSS.escape` requires an argument.');
+  }
+  let string = String(value);
+  let length = string.length;
+  let index = -1;
+  let codeUnit;
+  let result = '';
+  let firstCodeUnit = string.charCodeAt(0);
+
+  if (
+    // If the character is the first character and is a `-` (U+002D), and
+    // there is no second character, […]
+    length == 1 &&
+    firstCodeUnit == 0x002D
+  ) {
+    return '\\' + string;
+  }
+
+  while (++index < length) {
+    codeUnit = string.charCodeAt(index);
+    // Note: there’s no need to special-case astral symbols, surrogate
+    // pairs, or lone surrogates.
+
+    // If the character is NULL (U+0000), then the REPLACEMENT CHARACTER
+    // (U+FFFD).
+    if (codeUnit == 0x0000) {
+      result += '\uFFFD';
+      continue;
+    }
+
+    if (
+      // If the character is in the range [\1-\1F] (U+0001 to U+001F) or is
+      // U+007F, […]
+      (codeUnit >= 0x0001 && codeUnit <= 0x001F) || codeUnit == 0x007F ||
+      // If the character is the first character and is in the range [0-9]
+      // (U+0030 to U+0039), […]
+      (index == 0 && codeUnit >= 0x0030 && codeUnit <= 0x0039) ||
+      // If the character is the second character and is in the range [0-9]
+      // (U+0030 to U+0039) and the first character is a `-` (U+002D), […]
+      (
+        index == 1 &&
+        codeUnit >= 0x0030 && codeUnit <= 0x0039 &&
+        firstCodeUnit == 0x002D
+      )
+    ) {
+      // https://drafts.csswg.org/cssom/#escape-a-character-as-code-point
+      result += '\\' + codeUnit.toString(16) + ' ';
+      continue;
+    }
+
+    // If the character is not handled by one of the above rules and is
+    // greater than or equal to U+0080, is `-` (U+002D) or `_` (U+005F), or
+    // is in one of the ranges [0-9] (U+0030 to U+0039), [A-Z] (U+0041 to
+    // U+005A), or [a-z] (U+0061 to U+007A), […]
+    if (
+      codeUnit >= 0x0080 ||
+      codeUnit == 0x002D ||
+      codeUnit == 0x005F ||
+      codeUnit >= 0x0030 && codeUnit <= 0x0039 ||
+      codeUnit >= 0x0041 && codeUnit <= 0x005A ||
+      codeUnit >= 0x0061 && codeUnit <= 0x007A
+    ) {
+      // the character itself
+      result += string.charAt(index);
+      continue;
+    }
+
+    // Otherwise, the escaped character.
+    // https://drafts.csswg.org/cssom/#escape-a-character
+    result += '\\' + string.charAt(index);
+  }
+  return result;
 }
 
 function parseCookie(str, opts) {
@@ -569,6 +649,7 @@ function filter(obj, cb) {
 
 let utils = {
   concatUniq,
+  cssEscape,
   debounce,
   difference,
   estimateMaxEntropy,
