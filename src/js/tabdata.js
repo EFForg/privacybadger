858a4d834b4a49181c2a0b0b9e1e259154c14e32
Alexei <alexeiatyahoodotcom@gmail.com>
2024-05-28 17:17:45 -0400

    Remove blocking webRequest

--- src/js/tabdata.js
+++ src/js/tabdata.js
@@ -50,10 +50,6 @@ function TabData() {
    *         host: {?String}
    *         widgetReplacementReady: {Boolean}
    *         widgetQueue: {Array} widget objects
-   *         warAccessTokens: {
-   *           <extension_resource_URL>: {String} access token
-   *           ...
-   *         }
    *       },
    *       ...
    *     },
