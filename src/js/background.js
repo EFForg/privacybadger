582f873f847398ef75e0636c7465a50cb8e49fa3
Alexei <alexeiatyahoodotcom@gmail.com>
2024-05-28 17:17:34 -0400

    Remove static block and surrogate rulesets
    All tracker blocking and request modification is now done
    entirely with dynamic rules.
    
    This removes significant complexity.

--- src/js/background.js
+++ src/js/background.js
@@ -89,20 +89,6 @@ function Badger(from_qunit) {
   let widgetListPromise = widgetLoader.loadWidgetsFromFile(
     "data/socialwidgets.json").catch(console.error);
 
-  let seedBlockedBasesPromise = new Promise(resolve => {
-    self.seedBlockedBases = new Set();
-    fetch(constants.SEED_DATA_LOCAL_URL)
-      .then(resp => resp.json())
-      .then(seed => {
-        for (let base of Object.keys(seed.snitch_map)) {
-          if (seed.snitch_map[base].length >= constants.TRACKING_THRESHOLD) {
-            self.seedBlockedBases.add(base);
-          }
-        }
-        resolve();
-      });
-  });
-
   // we need to get ready to create dynamic rules before initializing storage
   getMaxDynamicRuleId(function (id) {
     self.maxDynamicRuleId = id;
@@ -144,9 +130,7 @@ function Badger(from_qunit) {
 
     // seed data loading depends on the yellowlist
     await pbconfigPromise;
-    // also get ready to create dynamic DNR rules,
-    // for which we need the set of blocked domains in seed data
-    await seedBlockedBasesPromise;
+    // also get ready to create dynamic DNR rules
     subscribeToActionMapUpdates();
     // now async load seed data
     let seedDataPromise = self.updateTrackerData().catch(console.error);
@@ -412,10 +396,6 @@ Badger.prototype = {
       console.error(err);
       throw new Error("Failed to parse seed data JSON");
     }
-
-    await chrome.declarativeNetRequest.updateEnabledRulesets({
-      enableRulesetIds: ['seed_ruleset', 'surrogates_ruleset']
-    });
 
     self.storage.mergeUserData(data);
   },
