3f970d9f03ea53a6e4d2b35c4a7e802cfd932075
Alexei <alexeiatyahoodotcom@gmail.com>
2024-05-28 17:17:24 -0400

    Remove blocked frame hiding/collapsing
    >Unlike the webRequest API, any images or iframes blocked using the
    >declarativeNetRequest API are automatically collapsed in the DOM.
    
    https://developer.chrome.com/docs/extensions/reference/declarativeNetRequest/

--- src/js/background.js
+++ src/js/background.js
@@ -849,7 +849,6 @@ Badger.prototype = {
     disableHyperlinkAuditing: true,
     disableNetworkPrediction: true,
     disableTopics: true,
-    hideBlockedElements: true,
     learnInIncognito: false,
     learnLocally: false,
     seenComic: false,
@@ -933,6 +932,7 @@ Badger.prototype = {
     if (self.isUpdate) {
       [
         "disableFloc",
+        "hideBlockedElements",
         "migrationLevel",
         "preventWebRTCIPLeak",
         "showTrackingDomains",
