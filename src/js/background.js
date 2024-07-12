2ba7716cc8b00454452526d4177f08f99b5deb5f
Alexei <alexeiatyahoodotcom@gmail.com>
2024-07-12 17:33:14 -0400

    Fix launching in MV3 Firefox

--- src/js/background.js
+++ src/js/background.js
@@ -244,9 +244,6 @@ Badger.prototype = {
     let excludeMatches = dnrUtils.convertSiteDomainsToMatchPatterns(
       prefs.getItem("disabledSites"));
 
-    // TODO "matchOriginAsFallback" and "world" are not supported in Firefox
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1853411
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1736575
     let scripts = constants.CONTENT_SCRIPTS
       .filter(item => {
         switch (item.id) {
