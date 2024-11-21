b7ea1db4faceaa65e67ddb9a0779790fe96ee4a2
lenacohen <lcohen@eff.org>
2024-11-21 11:15:01 -0500

    Add close icon to tooltip

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -59,6 +59,15 @@ function setTextDirection() {
     ['#fittslaw', '.overlay-close'].forEach((selector) => {
       toggle_css_value(selector, "float", "right", "left");
     });
+    // Workaround for tooltipster dynamically inserted after localization
+    let css = document.createElement("style");
+    css.textContent = `
+    #dismiss-tooltip {
+      float: left;
+      right: unset;
+      left: -5px;
+    }`;
+    document.body.appendChild(css);
 
   // options page
   } else if (document.location.pathname == "/skin/options.html") {
