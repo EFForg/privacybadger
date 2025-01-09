c9c75c4b6fa0f5e8e32be1a574d99784b1e77663
Alexei <alexeiatyahoodotcom@gmail.com>
2025-01-09 14:29:36 -0500

    Remove sender.origin fallbacks

--- src/js/webrequest.js
+++ src/js/webrequest.js
@@ -788,18 +788,11 @@ function dispatcher(request, sender, sendResponse) {
   // messages from content scripts are to be treated with greater caution:
   // https://groups.google.com/a/chromium.org/g/chromium-extensions/c/0ei-UCHNm34/m/lDaXwQhzBAAJ
   //
-  // prefer sender.origin when available
+  // use sender.origin, not sender.url:
   // https://issues.chromium.org/issues/40095810
   // https://bugzilla.mozilla.org/show_bug.cgi?id=1787379
   // https://github.com/uBlockOrigin/uBlock-issues/issues/1992#issuecomment-1058056302
-  //
-  // TODO remove sender.origin sender.url/request.frameUrl fallbacks
-  // TODO once minimum supported versions equal or exceed
-  // TODO 80 (Chromium) and 126 (Firefox) in all builds
-  if (utils.hasOwn(sender, "origin") ?
-    sender.origin == "null" || sender.origin + '/' !== chrome.runtime.getURL('') :
-    !sender.url.startsWith(chrome.runtime.getURL(''))) {
-
+  if (sender.origin == "null" || sender.origin + '/' !== chrome.runtime.getURL('')) {
     // reject unless it's a known content script message
     const KNOWN_CONTENT_SCRIPT_MESSAGES = [
       "allowWidgetOnSite",
@@ -845,8 +838,7 @@ function dispatcher(request, sender, sendResponse) {
   }
 
   case "checkClobberingEnabled": {
-    if (utils.hasOwn(sender, "origin") ?
-      sender.origin == "null" : request.frameUrl == "about:blank") {
+    if (sender.origin == "null") {
       return sendResponse();
     }
 
@@ -856,8 +848,7 @@ function dispatcher(request, sender, sendResponse) {
       return sendResponse();
     }
 
-    let frame_host = extractHostFromURL(
-      utils.hasOwn(sender, "origin") ? sender.origin + '/' : request.frameUrl);
+    let frame_host = extractHostFromURL(sender.origin + '/');
 
     // CNAME uncloaking
     if (utils.hasOwn(badger.cnameDomains, frame_host)) {
@@ -1029,26 +1020,19 @@ function dispatcher(request, sender, sendResponse) {
 
   case "supercookieReport": {
     if (badger.hasSupercookie(request.data)) {
-      let frame_host = extractHostFromURL(
-        utils.hasOwn(sender, "origin") ?
-          sender.origin + '/' : request.frameUrl);
-      if (frame_host) {
-        recordSupercookie(sender.tab.id, frame_host);
-      }
+      let frame_host = extractHostFromURL(sender.origin + '/');
+      recordSupercookie(sender.tab.id, frame_host);
     }
     break;
   }
 
   case "detectSupercookies": {
-    if (utils.hasOwn(sender, "origin") ?
-      sender.origin == "null" : request.frameUrl == "about:blank") {
+    if (sender.origin == "null") {
       return sendResponse();
     }
 
     let tab_host = extractHostFromURL(sender.tab.url),
-      frame_host = extractHostFromURL(
-        utils.hasOwn(sender, "origin") ?
-          sender.origin + '/' : request.frameUrl);
+      frame_host = extractHostFromURL(sender.origin + '/');
 
     // CNAME uncloaking
     if (utils.hasOwn(badger.cnameDomains, frame_host)) {
@@ -1066,10 +1050,7 @@ function dispatcher(request, sender, sendResponse) {
   case "detectFingerprinting": {
     if (sender.frameId > 0) {
       // do not modify the JS environment in Cloudflare CAPTCHA frames
-      if (utils.hasOwn(sender, "origin") ?
-        sender.origin === "https://challenges.cloudflare.com" :
-        sender.url.startsWith("https://challenges.cloudflare.com/")) {
-
+      if (sender.origin === "https://challenges.cloudflare.com") {
         sendResponse(false);
         break;
       }
@@ -1568,9 +1549,7 @@ function dispatcher(request, sender, sendResponse) {
     // implications of accepting pbSurrogateMessage events
     // from third-party scripts in nested frames
     if (sender.frameId > 0) {
-      let frame_origin = utils.hasOwn(sender, "origin") ?
-        sender.origin != "null" && sender.origin :
-        request.frameUrl && (new URL(request.frameUrl)).origin;
+      let frame_origin = sender.origin != "null" && sender.origin;
 
       if (!frame_origin) {
         break;
