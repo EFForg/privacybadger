5f5d437a162cfa18502629eecdd21b36616782b1
Alexei <alexeiatyahoodotcom@gmail.com>
2025-01-22 15:44:17 -0500

    Fix DNR-breaking extension update bug
    If we update to an extension version with a larger tracker database,
    and if we disabled PB on one or more sites, we run into a dynamic
    rule ID bookkeeping bug that causes tracker database DNR rule
    registration to fail entirely.
    
    We can avoid this bug by delaying all DNR rule updates until
    we are done resetting max ID as part of resetting tracker data.

--- src/js/background.js
+++ src/js/background.js
@@ -136,6 +136,10 @@ function Badger(from_qunit) {
       }
     });
 
+    await widgetListPromise;
+    await seedDataPromise;
+    await tabDataPromise;
+
     if (self.isUpdate && self.manifestVersion > 2) {
       // TODO don't need to do disabled sites on every update, just the first one to MV3 ...
       let disabledSites = self.getDisabledSites();
@@ -150,10 +154,6 @@ function Badger(from_qunit) {
         dnrUtils.updateWidgetSiteAllowlistRules(widgetSiteAllowlist);
       }
     }
-
-    await widgetListPromise;
-    await seedDataPromise;
-    await tabDataPromise;
 
     if (self.isFirstRun || self.isUpdate || !self.getPrivateSettings().getItem('doneLoadingSeed')) {
       // block all widget domains
