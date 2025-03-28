86c8d340473b4d92e6468d16676090e01da5046d
Alexei <alexeiatyahoodotcom@gmail.com>
2025-03-28 17:18:17 -0400

    Don't hold up initialization on tab data query
    If the mass tab query is slow for whatever reason, rather than
    fail completely, let's handle whatever specific issues arise
    from not having tab data.

--- src/js/background.js
+++ src/js/background.js
@@ -75,8 +75,9 @@ function Badger(from_qunit) {
     self.setPrivacyOverrides();
 
     // kick off async initialization steps
-    let pbconfigPromise = self.initPbconfig().catch(console.error),
-      tabDataPromise = self.tabData.initialize().catch(console.error);
+    let pbconfigPromise = self.initPbconfig().catch(console.error);
+
+    self.tabData.initialize().catch(console.error);
 
     // async load known CNAME domain aliases (but don't wait on them)
     self.initializeCnames().catch(console.error);
@@ -95,7 +96,6 @@ function Badger(from_qunit) {
     // wait for async functions (seed data, yellowlist, ...) to resolve
     await widgetListPromise;
     await seedDataPromise;
-    await tabDataPromise;
 
     if (self.isFirstRun || self.isUpdate || !self.getPrivateSettings().getItem('doneLoadingSeed')) {
       // block all widget domains
