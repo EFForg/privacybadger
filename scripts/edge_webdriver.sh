460510aa87e0124299d53dcf331d0e172a294087
Alexei <alexeiatyahoodotcom@gmail.com>
2025-07-16 14:37:35 -0400

    Fix Edge WebDriver installation

--- scripts/edge_webdriver.sh
+++ scripts/edge_webdriver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 edge_version_major=$(microsoft-edge-beta --product-version | cut -d . -f 1)
-edgedriver_version_url="https://msedgedriver.azureedge.net/LATEST_RELEASE_${edge_version_major}_LINUX"
+edgedriver_version_url="https://msedgedriver.microsoft.com/LATEST_RELEASE_${edge_version_major}_LINUX"
 edgedriver_version=$(curl -s "$edgedriver_version_url" | tr -d "\0\r\n" | cut -c 3-)
 if [ -z "$edgedriver_version" ]; then
   echo "Failed to retrieve Edge WebDriver version!"
@@ -9,7 +9,7 @@ if [ -z "$edgedriver_version" ]; then
 fi
 
 echo "Installing Edge WebDriver version $edgedriver_version ..."
-wget "https://msedgedriver.azureedge.net/${edgedriver_version}/edgedriver_linux64.zip"
+wget "https://msedgedriver.microsoft.com/${edgedriver_version}/edgedriver_linux64.zip"
 unzip edgedriver_linux64.zip
 sudo mv msedgedriver /usr/local/bin/
 sudo chmod a+x /usr/local/bin/msedgedriver
