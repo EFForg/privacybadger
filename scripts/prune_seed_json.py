43dafdc48eb02cb108d4907541a2737b10fb3a90
Alexei <alexeiatyahoodotcom@gmail.com>
2026-05-04 11:37:34 -0400

    Discard fp_scripts entries that we won't act on

--- scripts/prune_seed_json.py
+++ scripts/prune_seed_json.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 
 import json
+import subprocess
 import sys
 
 from collections import OrderedDict
+
+
+def prune_fp_scripts(data):
+    export_js = """
+// shim just enough for constants.js to load
+globalThis.chrome = { runtime: { getURL: ()=>{} } };
+const { default: constants } = await import('./src/js/constants.js');
+process.stdout.write(JSON.stringify(Array.from(constants.FP_CDN_DOMAINS)));"""
+
+    try:
+        cmd = ["node", "--experimental-default-type=module", f'--eval={export_js}']
+        fp_cdn_domains = subprocess.run(
+            cmd, capture_output=True, check=True, text=True).stdout.strip()
+    except subprocess.CalledProcessError as ex:
+        print(ex.stderr, file=sys.stderr)
+        raise ex
+
+    new_fp_scripts = {}
+    for domain in data['fp_scripts']:
+        if domain in fp_cdn_domains:
+            new_fp_scripts[domain] = data['fp_scripts'][domain]
+
+    data['fp_scripts'] = new_fp_scripts
+
+    return data
 
 
 def prune_action_map(data):
@@ -41,6 +67,8 @@ if __name__ == "__main__":
         seed_data = json.load(seed_file, object_pairs_hook=OrderedDict)
 
         seed_data = prune_action_map(seed_data)
+
+        seed_data = prune_fp_scripts(seed_data)
 
         # write the data back out
         seed_file.seek(0)
